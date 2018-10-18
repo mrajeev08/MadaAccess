c914ee710ec24ce8a63b2267f604591556727c4a
Malavika Rajeev <mrajeev08@gmail.com>
2018-10-18 18:04:26 -0400

    removing sync files

--- push_up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-## -r recurse through sub-directories
-## -L transform symlink into reference file/dir
-## -v be verbose about printing messages
-## -z compresses data before transfer and decompresses after transfer
-
-rsync -rLvz --update --exclude '*.git' --exclude '.Rproj*' --exclude archive --exclude '.gitignore' --exclude 'README.md' --exclude 'push_up.sh' --exclude lit ~/Documents/MadaAccess/ ~/Dropbox/MadaPEP/Mal_analyses/
