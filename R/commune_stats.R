a2ccca71259afcdc443148fc8b1a481f01d02fd8
Malavika Rajeev <mrajeev08@gmail.com>
2018-10-19 00:31:36 -0400

    fixed for running with openMPI on della

--- R/commune_stats.R
+++ R/commune_stats.R
@@ -1,11 +1,17 @@
-## libraries for gis
+## Init MPI Backend
+## ################
 rm(list = ls())
+library(doMPI)
+cl <- startMPIcluster()
+clusterSize(cl) # this just tells you how many you've got
+registerDoMPI(cl)
+
+## libraries
 library(rgdal)
 library(raster)
 library(malariaAtlas)
 library(gdistance)
 library(foreach)
-library(doMPI)
 library(doRNG)
 source("R/travel_times.R")
 
@@ -43,47 +49,43 @@ point_mat <- as.matrix(gps_locs@coords)
 travel_times <- get.travel.times(friction = friction, shapefile = mada_communes, 
                                  coords = point_mat, 
                                  trans_matrix_exists = TRUE, 
-                                 filename_trans = "trans_gc.rds")
+                                 filename_trans = "output/trans_gc.rds")
 
 ttimes <- raster::extract(travel_times, mada_communes, fun = mean, 
                           weights = TRUE, normalizeWeights = TRUE,
                           na.rm=TRUE, df = TRUE, sp = FALSE)
-  
+
 ## getting catchments
 catch_mat <- matrix(NA, nrow = nrow(mada_communes), ncol = nrow(point_mat))
-rownames(catch_mat) <- mada_communes$name_3
+rownames(catch_mat) <- mada_communes$mdg_com_co
 colnames(catch_mat) <- gps_locs$CTAR
 
-## Init MPI Backend
-## ################
-cl <- startMPIcluster()
-clusterSize(cl) # this just tells you how many you've got
-registerDoMPI(cl)
-
-foreach (i = 1:nrow(point_mat)) %dopar% {
+for(i in (1:nrow(point_mat))) {
+  print(i)
+  print(Sys.time())
   point_mat_sub <- t(as.matrix(point_mat[i, ]))
   travel_time_pt <- get.travel.times(friction = friction, shapefile = mada_communes, 
-                                   coords = point_mat_sub, 
-                                   trans_matrix_exists = TRUE, 
-                                   filename_trans = "output/trans_gc.rds")
-  mada.out <- raster::extract(travel_times, mada_communes, fun = mean, 
+                                     coords = point_mat_sub, 
+                                     trans_matrix_exists = TRUE, 
+                                     filename_trans = "output/trans_gc.rds")
+  mada.out <- raster::extract(travel_time_pt, mada_communes, fun = mean, 
                               weights = TRUE, normalizeWeights = TRUE,
                               na.rm=TRUE, df = TRUE, sp = FALSE)
   catch_mat[, i] <- mada.out[, 2]
 }
 
 # getting min ttimes
-inds = apply(catch_mat, 2, function (x) (which(x == min(x), arr.ind=TRUE)))
+inds = apply(catch_mat, 2, function (x) (which(x == min(x[is.finite(x)]), arr.ind=TRUE)))
 catchments <- as.data.frame(cbind(rownames(catch_mat), colnames(catch_mat[, inds])))
 
 ## add pop + baseline travel time to each commune
 pop2015adj <- raster::extract(mada_pop2015adj, mada_communes, fun = mean, 
-                          weights = TRUE, normalizeWeights = TRUE,
-                          na.rm=TRUE, df = TRUE, sp = FALSE)
+                              weights = TRUE, normalizeWeights = TRUE,
+                              na.rm=TRUE, df = TRUE, sp = FALSE)
 
 pop2020adj <- raster::extract(mada_pop2020adj, mada_communes, fun = mean, 
-                           weights = TRUE, normalizeWeights = TRUE,
-                           na.rm=TRUE, df = TRUE, sp = FALSE)
+                              weights = TRUE, normalizeWeights = TRUE,
+                              na.rm=TRUE, df = TRUE, sp = FALSE)
 
 ## output dataframe with catchments + ttimes + pop!
 master <- as.data.frame(cbind(ttimes, catchments[, 2], pop2015adj[, 2], 
@@ -91,10 +93,12 @@ master <- as.data.frame(cbind(ttimes, catchments[, 2], pop2015adj[, 2],
 
 names(master) <- c("communeID", "ttime", "catchment", "pop2015adj", "pop2020adj")
 
+save(master, catch_mat, ttimes, file = "output/commune_data.rda")
+
 write.csv(master, "output/commune_data.csv")
 
 ### Then just close it out at the end
 closeCluster(cl)
 mpi.quit()
-  
+Sys.time()
   
