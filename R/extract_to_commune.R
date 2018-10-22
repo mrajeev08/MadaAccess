f21c6b566fdfb6e1abaf5eb55ddec76566f809d8
Malavika Rajeev <mrajeev08@gmail.com>
2018-10-22 16:59:36 -0400

    split scripts
    split scripts so that one each for generating catchment and extracting to commune level (both in cluster) and getting the catchments (locally)

--- R/extract_to_commune.R
+++ R/extract_to_commune.R
@@ -11,26 +11,21 @@ registerDoMPI(cl)
 # libraries for gis
 library(raster)
 library(maptools)
-library(maps)
-library(GISTools)
 library(rgdal)
-library(sp)
-library(rgdal)
-library(gdistance)
-ttime <- raster("output/ttimes_all.tif")
 mada_communes <- readOGR("data/MadaGIS/commune_mada.shp")
 
 ## data from world pop
+ttimes <- raster("output/ttimes_all.tif")
 mada_pop2015adj <- raster("data/WorldPop/MDG_ppp_2015_adj_v2/MDG_ppp_2015_adj_v2.tif")
 mada_pop2020adj <- raster("data/WorldPop/MDG_ppp_2020_adj_v2/MDG_ppp_2020_adj_v2.tif")
 
-mada.out <- raster::extract(ttimes, mada_communes, fun = mean, na.rm=TRUE, df = TRUE, sp = TRUE)
+mada_out <- raster::extract(ttimes, mada_communes, fun = mean, na.rm=TRUE, df = TRUE, sp = TRUE)
 
-mada.out <- raster::extract(mada_pop2015adj, mada_out, fun = sum, na.rm=TRUE, df = TRUE, sp = TRUE)
+mada_out <- raster::extract(mada_pop2015adj, mada_out, fun = sum, na.rm=TRUE, df = TRUE, sp = TRUE)
 
-mada.out <- raster::extract(mada_pop2020adj, mada_out, fun = sum, na.rm=TRUE, df = TRUE, sp = TRUE)
+mada_out <- raster::extract(mada_pop2020adj, mada_out, fun = sum, na.rm=TRUE, df = TRUE, sp = TRUE)
 
-writeSpatialShape(mada.out, "output/communes/communes_extract.shp")
+writeSpatialShape(mada_out, "output/communes/communes_extract.shp")
 
 ### Then just close it out at the end
 closeCluster(cl)
