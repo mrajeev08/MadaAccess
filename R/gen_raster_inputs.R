3fe8aab987d04ecaee203d5e6dd695705f0a6b68
Malavika Rajeev <mrajeev08@gmail.com>
2018-11-04 23:25:50 -0500

    try at aligning rasters with crop and extend methods
    does not work because only crops whole cells!

--- R/gen_raster_inputs.R
+++ R/gen_raster_inputs.R
@@ -8,14 +8,20 @@ library(malariaAtlas)
 
 ## raster layers
 ttimes <- raster("output/ttimes_all.tif")
-pop <- raster("data/MDG_ppp_2015_adj_v2/MDG_ppp_2015_adj_v2.tif")
-pop10 <- aggregate(pop, fact = 10, fun = sum) # aggregate to same res
-pop10_resamp <- resample(pop10, ttimes)
-writeRaster(pop10_resamp, "output/pop2015_resamp.tif", overwrite = TRUE)
+pop <- raster("data/WorldPop/MDG_ppp_2015_adj_v2/MDG_ppp_2015_adj_v2.tif")
+pop10 <- aggregate(pop, fact = 10, fun = sum, na.rm = TRUE)
+ttimes_pol <- rasterToPolygons(ttimes)
+# pop10 <- extract(pop, ttimes_pol, fun = sum, na.rm = TRUE)
 
-## getting write friction surface
-mada_communes <- readOGR("data/MadaGIS/commune_mada.shp")
-friction <- malariaAtlas::getRaster(
-surface = "A global friction surface enumerating land-based travel speed for a nominal year 2015")
-friction_mada <- crop(friction, mada_communes)
-writeRaster(friction_mada, "output/friction_mada.tif", overwrite = TRUE)
+e <- intersect(extent(ttimes), extent(pop10)) # get minimum extent
+ttimes <- raster::crop(ttimes, e)
+pop10 <- crop(pop10, e)
+pop10 <- shift(pop10, x = (origin(ttimes) - origin(pop10))[1], y = (origin(ttimes) - origin(pop10))[2])
+ttimes_weighted <- ttimes*pop10
+
+# ## getting friction surface
+# mada_communes <- readOGR("data/MadaGIS/commune_mada.shp")
+# friction <- malariaAtlas::getRaster(
+# surface = "A global friction surface enumerating land-based travel speed for a nominal year 2015")
+# friction_mada <- crop(friction, mada_communes)
+# writeRaster(friction_mada, "output/friction_mada.tif", overwrite = TRUE)
