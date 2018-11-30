0f9626aed270b242b9bbe2b5bd0c3b5f9aaf78dd
Malavika Rajeev <mrajeev08@gmail.com>
2018-11-30 13:49:19 -0500

    script to get masked and unmasked (!) friction surface from MAP
    ! The unmasked one takes forever!!

---
+++ R/friction_files.R
@@ -0,0 +1,21 @@
+## Getting masked and unmasked friction surfaces
+rm(list=ls())
+library(malariaAtlas)
+library(raster)
+
+# Masked from MAP
+mada_communes <- getShp(country = "Madagascar", admin_level = "admin3") # get commune level shapefile
+friction_masked <- malariaAtlas::getRaster(
+    surface = "A global friction surface enumerating land-based travel speed for a nominal year 2015",
+    shp = mada_communes)
+plot(friction_masked)
+writeRaster(friction_masked, "output/friction_mada_masked.tif", overwrite = TRUE)
+
+## Unmasked
+
+## get unmasked file from MAP (takes a long time! Big file...)
+friction_masked <- malariaAtlas::getRaster(
+  surface = "A global friction surface enumerating land-based travel speed for a nominal year 2015")
+friction_unmasked <- crop(friction_unmasked, mada_communes)
+plot(friction_unmasked)
+writeRaster(friction_unmasked, "output/friction_mada_unmasked.tif", overwrite = TRUE)
