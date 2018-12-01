93972749acea403d4b39d396d6899011f0529900
Malavika Rajeev <mrajeev08@gmail.com>
2018-12-01 11:17:35 -0500

    written to run parallel using doMPI

--- R/get.catchmat.R
+++ R/get.catchmat.R
@@ -1,28 +1,33 @@
 ### Function for getting catchment matrix
 ## Admin level average or weighted average by pop for each clinic
     
-get.catchmat <- function(point_mat, fric, shape, place_names, point_names, admin = "district",
+get.catchmat <- function(point_mat, fric, shape, admin = "district",
                          pop_rast, pop_pol, weighted = TRUE, type = "masked"){
-    ## getting catchments
-    catch_mat <- matrix(NA, nrow = nrow(shape), ncol = nrow(point_mat))
-    rownames(catch_mat) <- place_names
-    colnames(catch_mat) <- point_names
     
-    foreach(i = 1:nrow(point_mat),.packages = c('raster', 'rgdal', 'sp', 'gdistance'), 
-            .errorhandling = 'stop',
-            .export = c("point_mat", "fric", "shape", "place_names", "point_names", "admin",
-                        "pop_rast", "pop_pol", "weighted", "type", 'get.travel.times')
+  ## getting catchments
+    catch_mat <- foreach(coords = iter(point_mat,"row"),
+                         .packages = c('raster', 'rgdal', 'sp', 'gdistance'),
+                         .errorhandling = 'stop',
+                         .export = 'get.travel.times',
+                         .combine = "cbind"
     ) %dopar% {
-      print(i)
+      print(coords)
       print(Sys.time())
-      point_mat_sub <- t(as.matrix(point_mat[i, ]))
+      point_mat_sub <- as.matrix(coords)
+      
+      if (type == "masked"){
+        trans_mat <- "output/trans_gc_masked.rds"
+      } else {
+        trans_mat = "output/trans_gc_unmasked.rds"
+      }
+      
       travel_time_pt <- get.travel.times(friction = fric, shapefile = shape, 
                                      coords = point_mat_sub, 
-                                     trans_matrix_exists = FALSE, 
-                                     filename_trans = "output/trans_gc_test.rds")
+                                     trans_matrix_exists = TRUE, 
+                                     filename_trans = trans_mat)
 
       if (weighted == TRUE){
-        ## add 1e6 to 0 ttimes
+        ## add 1e-6 to 0 ttimes
         travel_time_pt[travel_time_pt == 0] <- 1e-6
         weighted_ttimes <- travel_time_pt*pop_rast
         names(weighted_ttimes) <- "w_ttimes"
@@ -34,7 +39,7 @@ get.catchmat <- function(point_mat, fric, shape, place_names, point_names, admin
         out <- raster::extract(travel_time_pt, shape, fun = mean, 
                                na.rm = TRUE, df = TRUE, sp = TRUE)
       }
-      catch_mat[, i] <- out$ttimes
+      out$ttimes 
     }
     
     write.csv(catch_mat, paste0("output/", admin, "_", "catchmat_", type, "_", 
