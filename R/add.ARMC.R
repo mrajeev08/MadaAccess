333665d14694d5c9b3b8c63c53298d3698bc9dfa
Malavika Rajeev <mrajeev08@gmail.com>
2019-02-01 15:10:52 -0500

    incremental scenario analyses (running on cluster)

--- R/add.ARMC.R
+++ R/add.ARMC.R
@@ -3,39 +3,61 @@
 add.armc <- function(current_ARMC, candidate_ARMC, prop_pop, threshold, delta_tt_min = 1e-5,
                      steps = 1000, base_prop, 
                      friction, shapefile, filename_trans) {
-  
+  ## to test
+  # current_ARMC = gps_locs; candidate_ARMC = csbs[seq(1, 1000, by = 100), ];
+  # threshold = 3; delta_tt_min = 1e-20;
+  # steps = 10;
+  # friction = friction_unmasked; shapefile = mada_district;
+  # filename_trans = "output/trans_gc_unmasked.rds";
+
   ## For the number of possible clinics you could add
-  prop_under <- rep(NA, nrow(candidate_ARMC))
-  new_ARMC <- data.frame(CTAR = NA, X_COORD = NA, Y_COORD = NA) ## empty data frame to bind to
+  prop_under <- rep(NA, ncol(candidate_ARMC))
+  cands <- nrow(candidate_ARMC)
+  new_ARMC <- data.frame(CTAR = rep(NA, cands), X_COORD = rep(NA, cands), Y_COORD = rep(NA, cands))
+
+  ## empty data frame to bind to
   
   for (i in 1:steps) {
-    while (nrow(candidate_ARMC) > 0) {
+    if (nrow(candidate_ARMC) > 0) {
       ## Iterate through and find the one that when you add it reduces travel times the most
-      ranked_coords <- foreach(j = iter(candidate_ARMC, "row"),
+      ranked_coords <- foreach(j = 1:nrow(candidate_ARMC),
                          .packages = c('raster', 'rgdal', 'sp', 'gdistance'),
                          .errorhandling = 'stop',
-                         .export = 'get.travel.times',
-                         .combine = "c"
+                         .export = 'get.travel.times', 
+                         .combine = c
       ) %dopar% {
         point_mat <- rbind(current_ARMC, candidate_ARMC[j, ])
         point_mat <- as.matrix(cbind(point_mat$Y_COORD, point_mat$X_COORD)) # matrix of long and lat
-        times <- get.travel.times(friction, shapefile, coords = point_mat, trans_matrix_exists = TRUE, 
+        ttimes <- get.travel.times(friction, shapefile, coords = point_mat, trans_matrix_exists = TRUE, 
                                  filename_trans)
         
         ## proportion of pop living greater than threshold hrs away from CTAR (defaults to 3 hrs)
         sum(prop_pop[ttimes >= 60*threshold], na.rm = TRUE) 
       }
       
-      ## rank the one that minimizes this
-      prop_under[i] <- min(ranked_coords, na.rm = TRUE)
-      new_armc <- rbind(new_ARMC, candidate_ARMC[which(ranked_coords == min(ranked_coords, na.rm = TRUE)), ])
-      candidate_ARMC <- candidate_ARMC[-which(ranked_coords == min(ranked_coords, na.rm = TRUE)), ] 
-      ## remove top ranked
-      candidate_ARMC <- candidate_ARMC[-which(ranked_coords - base_prop <= delta_tt_min), ] 
+      if(min(ranked_coords, na.rm = TRUE)[1] != Inf){
+        ## rank the one that minimizes this and add to current armc
+        current_ARMC <- rbind(current_ARMC, 
+                              candidate_ARMC[which(ranked_coords == min(ranked_coords, na.rm = TRUE)[1]), ])
+        
+        ## add prop_under to the top ranked at the row i + 1 (because of place holder NA row)
+        prop_under[i] <- min(ranked_coords, na.rm = TRUE)[1]
+        
+        ## add to new armc list
+        new_ARMC[i, ] <- candidate_ARMC[which(ranked_coords == min(ranked_coords, na.rm = TRUE)[1]), ]
+      }
+      
+      ## remove top ranked and also will result in no rows if ranked_coords is all NA
+      candidate_ARMC <- candidate_ARMC[-which(ranked_coords == min(ranked_coords, na.rm = TRUE)[1]), ]
+      
+      # candidate_ARMC <- candidate_ARMC[-which(ranked_coords - base_prop <= delta_tt_min), ] 
       ## remove all which don't reduce above a certain threshold
     }
+    else{
+      next()
+    }
   }
-  new_armc$prop_under <- prop_under
-  return(new_armc)
+  new_ARMC$prop_under <- prop_under
+  return(new_ARMC)
 }
 
--- R/scenario_incremental.R
+++ R/scenario_incremental.R
@@ -1,4 +1,5 @@
 ## Scenario analysis, incremental parallelized
+
 ## Init MPI Backend
 rm(list = ls())
 # args <- commandArgs(trailingOnly = TRUE)
@@ -23,7 +24,7 @@ source("R/add.ARMC.R")
 mada_district <- readOGR("data/MadaGIS/district_init.shp")
 friction_masked <- raster("output/friction_mada_masked.tif")
 friction_unmasked <- raster("output/friction_mada_unmasked.tif")
-ttimes <- raster("output/ttimes_masked.tif")
+ttimes_base <- raster("output/ttimes_masked.tif")
 
 ## proportion of pop
 pop10 <- raster("output/pop10.tif")
@@ -31,15 +32,15 @@ prop_pop <- pop10/sum(values(pop10), na.rm = TRUE)
 
 ## baseline proportion
 threshold = 3 # threshold # of hours to calculate the proportion of pop at
-base_prop <- sum(prop_pop[ttimes >= 60*threshold], na.rm = TRUE)
+base_prop <- sum(prop_pop[ttimes_base >= 60*threshold], na.rm = TRUE)
 
 ## Existing points
 ## Point locations
-gps_locs <- read.csv(file = "data/ctar_metadata.csv")[,c(1, 3, 4)]
+gps_locs <- read.csv(file = "data/ctar_metadata.csv", stringsAsFactors = FALSE)[,c(1, 3, 4)]
 names(gps_locs) <- c ("CTAR", "X_COORD", "Y_COORD")
 
 ## candidate points
-csbs <- read.csv("data/csbs.csv")
+csbs <- read.csv("data/csbs.csv", stringsAsFactors = FALSE)
 csbs %>% 
   filter(type == "CSB2", genre_fs != "Priv", type_fs != "Health Post") %>%
   dplyr::select(CTAR = nom_fs, X_COORD = ycoor, Y_COORD = xcoor) -> csbs
@@ -50,10 +51,15 @@ csbs %>%
 # plot(csbs$xcoor, csbs$ycoor)
 
 ## check just with 10 
-check <- add.armc(current_ARMC = gps_locs, candidate_ARMC = csbs[seq(1, 1000, by = 100), ], prop_pop, 
-                  threshold, delta_tt_min = 1e-5,
-                  steps = 10, base_prop, 
-                  friction = friction_masked, shapefile = mada_district, 
-                  filename_trans = "output/trans_gc_masked.rds")
+system.time({
+  scenario <- add.armc(current_ARMC = gps_locs, candidate_ARMC = csbs, prop_pop, 
+                  threshold = 3, delta_tt_min = 1e-20,
+                  steps = nrow(csbs), base_prop, 
+                  friction = friction_unmasked, shapefile = mada_district, 
+                  filename_trans = "output/trans_gc_unmasked.rds")
+})
+write.csv(scenario, "output/scenario_incremental_prop3hrs_masked.csv")
 
-write.csv(check, "output/check_incremental.csv")
+### Then just close it out at the end
+closeCluster(cl)
+mpi.quit()
