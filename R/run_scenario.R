69aff0ada50f4190845c581cd131b5067084aae7
Malavika Rajeev <mrajeev08@gmail.com>
2019-02-11 15:55:38 -0500

    starting code for estimating burden

---
+++ R/run_scenario.R
@@ -0,0 +1,17 @@
+## Calculate incremental decrease
+## Run in parallel (should only take apprx 50 min or so)
+
+## 1. Read in ordered ctar
+
+## for i in 32:nrow(ctars)
+## calculate ttimes
+## summarize by commune + district using shapefile rasterized data 
+
+## append to dataframe and output
+
+## estimate burden @ district and commune level
+
+## calculate max + min # of bites on avg + p_rabid + expected bite incidence by ttimes + get mean deaths
+## (also do it stochastically?)
+
+## test to see if this gets you about the right # as the stochastic sims?
