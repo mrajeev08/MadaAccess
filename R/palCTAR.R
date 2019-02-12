df7e4e035991cf068e5fe3eba4e5a1208b08f89e
Malavika Rajeev <mrajeev08@gmail.com>
2019-02-12 14:37:22 -0500

    combined all travel time functions into one script file

--- R/palCTAR.R
+++
@@ -1,6 +0,0 @@
-library(scales)
-pal <- c("#FE6F32", "#2C715D", "#A5C3BC", "#4F82CD", "#B0C5DF", "#8B69D0", "#66c2a5", "#032478", "#724471", "#FEBC33", "#000303", "#E74A26", "#dd1c77", "#67001f", "#800026", "#ffffcc", "#014636", "#737373", "#d9d9d9", "#810f7c", "#41b6c4", "#edf8b1", "#bf812d", "#8c510a", "#7fbc41", "#f4a582", "#92c5de", "#ffffff", "#cab2d6", "#fddaec", "#e5d8bd")
-         
-ctar <- c("Ambatomainty", "Ambatondrazaka", "Ambositra", "Ambovombe Androy ", "Antsirabe", "Antsiranana", "Antsohihy", "Bekily", "Fianarantsoa", "Ihosy", "IPM", "Maevatanana", "Mahajanga", "Maintirano", "Manakara", "Mandritsara", "Manja", "Maroantsetra", "Marolambo", "Miarinarivo", "Moramanga", "Morondava", "Nosy be", "Sainte Marie", "Sambava", "Soanierana Ivongo", "Taolagnaro", "Toamasina", "Toliara", "Tsiroanomandidy", "Vangaindrano")
-
-scales::show_col(pal)
