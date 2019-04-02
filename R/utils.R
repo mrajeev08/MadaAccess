bbe08f1da1e9fd1a73f9b8bd670cc6134073addf
Malavika Rajeev <mrajeev08@gmail.com>
2019-04-02 10:31:04 +0300

    adding ggplot helpers for reordering

--- R/utils.R
+++ R/utils.R
@@ -192,3 +192,19 @@ add.alpha <- function(col, alpha = 1){
         function(x) 
           rgb(x[1], x[2], x[3], alpha = alpha))  
 }
+
+## ggplot helpers
+reorder_within <- function(x, by, within, fun = mean, sep = "___", ...) {
+  new_x <- paste(x, within, sep = sep)
+  stats::reorder(new_x, by, FUN = fun)
+}
+
+scale_x_reordered <- function(..., sep = "___") {
+  reg <- paste0(sep, ".+$")
+  ggplot2::scale_x_discrete(labels = function(x) gsub(reg, "", x), ...)
+}
+
+scale_y_reordered <- function(..., sep = "___") {
+  reg <- paste0(sep, ".+$")
+  ggplot2::scale_y_discrete(labels = function(x) gsub(reg, "", x), ...)
+}
