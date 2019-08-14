f97f49385c0dba900111382fb05bef46bf9d71be
Malavika Rajeev <mrajeev08@gmail.com>
2019-08-14 14:33:26 -0400

    added findy.bydate function

--- R/functions/utils.R
+++ R/functions/utils.R
@@ -208,3 +208,32 @@ scale_y_reordered <- function(..., sep = "___") {
   reg <- paste0(sep, ".+$")
   ggplot2::scale_y_discrete(labels = function(x) gsub(reg, "", x), ...)
 }
+
+## Find nth most recent file
+find.bydate <- function(path, patt, ind, rank){
+  ## function to get latest file
+  ## path = the folder to look in, pattern = the pattern to match to
+  ## ind = where the date is stored in the name if you were to strsplit by '_'
+  ## rank = which file do you want? most recent = 1, second most recent = 2, etc.
+  ## will print warning if rank is greater than the # of files, but will give you the oldest one
+  
+  files <- list.files(path, pattern = patt)
+  
+  if (rank > length(files)){
+    print("Warning: rank greater than # of files, returning oldest file")
+    rank <- length(files)
+  }
+  
+  time_stamp <- rep (NA, length(files))
+  
+  for (i in 1:length(files)){
+    time_stamp[i] <- unlist(strsplit(files[i], "_"))[ind]
+  }
+  
+  time_stamp <- substr(as.character(time_stamp), 1, 8)
+  diff <- as.numeric(Sys.Date() - as.Date(time_stamp, "%Y%m%d"))
+  files <- files[order(diff)]
+  retrieved <- read.csv(paste0(path, files[rank]), fileEncoding = 'latin1')
+  print(paste0(path, files[rank]))
+  return(retrieved)
+}
