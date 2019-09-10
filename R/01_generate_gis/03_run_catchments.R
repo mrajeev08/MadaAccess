dbe057f67d677345b152ac8a3ca5e8825d1cb8fb
Malavika Rajeev <mrajeev08@gmail.com>
2019-09-10 12:23:37 -0400

    corrected variable names for pop1x1

--- R/01_generate_gis/03_run_catchments.R
+++ R/01_generate_gis/03_run_catchments.R
@@ -1,8 +1,8 @@
 ####################################################################################################
 ##' Step 3: Generating catchment matrix
 ##' Details: Getting travel times to each clinic for each admin unit 
-##'   Needs(!) to be run in parallel to limit compute time
-##'   On the Della cluster at Princeton with NN cores, it takes approximately XX hrs
+##'   Recommended to run in parallel to limit compute time
+##'   On the Della cluster at Princeton with 32 cores, it takes approximately 6 minutes
 ##' Author: Malavika Rajeev 
 ####################################################################################################
 
@@ -24,15 +24,15 @@ library(malariaAtlas)
 library(gdistance)
 library(doRNG)
 library(foreach)
-source("R/ttime_functions.R")
+source("R/functions/ttime_functions.R")
 
 ##' Shapefiles from Malaria Atlas and CTAR point matrix
-mada_communes <- readOGR("data/shapefiles/communes.shp")
-mada_districts <- readOGR("data/shapefiles/districts.shp")
-pop1x1<- raster("output/worldpop2015adj_mada_1x1km.tif")
-point_mat <- read.csv("output/point_mat_CTAR.csv")
-friction_mada_unmasked <- raster("output/friction_mada_unmasked.tif")
-friction_mada_masked <- raster("output/friction_mada_masked.tif")
+mada_communes <- readOGR("data/processed/shapefiles/communes.shp")
+mada_districts <- readOGR("data/processed/shapefiles/districts.shp")
+pop1x1<- raster("data/processed/worldpop2015adj_mada_1x1km.tif")
+point_mat <- read.csv("data/processed/point_mat_CTAR.csv")
+friction_mada_unmasked <- raster("data/raw/friction_mada_unmasked.tif")
+friction_mada_masked <- raster("data/raw/friction_mada_masked.tif")
 
 ##' Getting catchment matrix masked and unmasked
 ##' This function generates travel times to each CTAR for each admin unit (district or commune)
@@ -41,20 +41,20 @@ friction_mada_masked <- raster("output/friction_mada_masked.tif")
 print(paste(Sys.time(), ": started generating district catchmats"))
 dist_mat_unmasked <- get.catchmat(point_mat = point_mat, fric = friction_mada_unmasked, 
                          shape = mada_districts, admin = "district", pop_rast = pop1x1, 
-                         pop_pol = mada_district$pop1x1, weighted = TRUE, type = "unmasked")
+                         pop_pol = mada_districts$pop, weighted = TRUE, type = "unmasked")
 dist_mat_masked <- get.catchmat(point_mat = point_mat, fric = friction_mada_masked, 
                                 shape = mada_districts, admin = "district", pop_rast = pop1x1, 
-                                pop_pol = mada_district$pop1x1, weighted = TRUE, type = "masked")
+                                pop_pol = mada_districts$pop, weighted = TRUE, type = "masked")
 print(paste(Sys.time(), ": finished generating district catchmats"))
 
 ##' Communes
 print(paste(Sys.time(), ": started generating commune catchmats"))
 comm_mat_unmasked <- get.catchmat(point_mat = point_mat, fric = friction_mada_unmasked, 
                                   shape = mada_communes, admin = "commune", pop_rast = pop1x1, 
-                                  pop_pol = mada_communes$pop1x1, weighted = TRUE, type = "unmasked")
+                                  pop_pol = mada_communes$pop, weighted = TRUE, type = "unmasked")
 comm_mat_masked <- get.catchmat(point_mat = point_mat, fric = friction_mada_masked, 
                                 shape = mada_communes, admin = "commune", pop_rast = pop1x1, 
-                                pop_pol = mada_communes$pop1x1, weighted = TRUE, type = "masked")
+                                pop_pol = mada_communes$pop, weighted = TRUE, type = "masked")
 print(paste(Sys.time(), ": finished generating commune catchmats"))
 
 ##' Close out cluster
