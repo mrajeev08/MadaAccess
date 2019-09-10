bbe0d4f560b77887da8c38b18d5b922432771093
Malavika Rajeev <mrajeev08@gmail.com>
2019-09-10 12:43:47 -0400

    removing match threshold
    now this function returns the best exact and partial matches with minimum distances

--- R/functions/match.names.R
+++ R/functions/match.names.R
@@ -11,27 +11,24 @@
 #' Can be used with a nesting category (i.e. matching commune names within each district name)
 #' or for matching names in general. Provide vectors of names in data (data_names) to a vector of names
 #' to match to (match_names) and a vector of shared nesting names (data_nest and match_nest). 
-#' The match_method comes from stringdistmatrix. Uses exact and then partial matching. If no matches below
-#' a certain threshold, then uses partial. 
+#' The match_method comes from stringdistmatrix. Uses exact and then partial matching. 
 #' 
 #' @param data_names vector of names in data that need to be matched
 #' @param data_nest vector of corresponding nesting factor for the names in the data 
 #' @param match_names vector of potential matches 
 #' @param match_nest vector of corresponding nesting factor for the potential matches
-#' @param match_threshold numeric, the threshold above which partial matching is used
 #' @param match_method  character string either "osa" "lv" or "dl. see Details. 
 #' @param nested
 #' 
-#' @return Dataframe with minimum distance and matches returned in columns ranked by distance 
+#' @return Dataframe with minimum distance and best match for fixed and partial matching and 
+#' the ranked fixed matches returned in columns (ranked by distance)
 #' @section Dependencies: stringdist, dplyr
 
-match.admin <- function(data_names, data_nest, match_names, match_nest, 
-                        match_threshold, match_method, nested = TRUE) {
+match.admin <- function(data_names, data_nest, match_names, match_nest, match_method, nested = TRUE) {
   
   require(stringdist)
   
   ## trouble shooting
-  # match_threshold = 5
   # match_method = "osa"
   # 
   # ## For nested = TRUE
@@ -75,27 +72,24 @@ match.admin <- function(data_names, data_nest, match_names, match_nest,
         ranked_matches <- t(apply(distance, 1, function(x) matches[order(x)]))
         min_dist <- apply(distance, 1, min)
         
+        ## also partial distances 
         partial <- adist(tomatch, matches, partial = TRUE)
         min_part <- apply(partial, 1, min)
         partial_matches <- t(apply(partial, 1, function(x) matches[order(x)]))
         
-        ## only keep if the partial matches if it meets the following conditions
-        ## 1. it is less than the length of the name to match (- 1)
-        ## 2. it is less than the minimum of the lev. distances
-        inds_to_rep <- which(
-          min_part <= min_dist & min_part < nchar(tomatch) - 1)
-        ranked_matches[inds_to_rep, ] <-  partial_matches[inds_to_rep, ]      
-        min_dist[inds_to_rep] <- min_part[inds_to_rep]
-        
         ## make df
-        df <- data.frame(list(names_tomatch = tomatch, nest = unique_nests[i], min = min_dist, 
+        df <- data.frame(list(names_tomatch = tomatch, nest = unique_nests[i],
+                              fixed_best = ranked_matches[, 1], min_fixed = min_dist,
+                              partial_best = partial_matches[, 1], min_partial = min_part, 
                               ranked_matches))
       }
       
       if(length(matches) == 1) {
         df <- data.frame(list(names_tomatch = tomatch, nest = unique_nests[i],
-                                min = NA, match = matches))
+                              fixed_best = matches, min_fixed = NA,
+                              partial_best = matches, min_partial = NA))
       }
+      
       match_df <- bind_rows(match_df, df)
     }
   } else {
@@ -103,25 +97,21 @@ match.admin <- function(data_names, data_nest, match_names, match_nest,
     tomatch <- unique(tolower(data_names))
     matches <- tolower(match_names)
     
-    ## do simple lev. distances with stringdistmatrix with a match threshold
+    ## do simple lev. distances with stringdistmatrix
     distance <- stringdistmatrix(tomatch, matches, method = match_method)
     ranked_matches <- t(apply(distance, 1, function(x) matches[order(x)]))
     min_dist <- apply(distance, 1, min)
     
+    ## then partial matching
     partial <- adist(tomatch, matches, partial = TRUE)
     min_part <- apply(partial, 1, min)
     partial_matches <- t(apply(partial, 1, function(x) matches[order(x)]))
-    
-    ## only keep if the partial matches if it meets the following conditions
-    ## 1. it is less than the length of the name to match (- 1)
-    ## 2. it is less than the minimum of the lev. distances
-    inds_to_rep <- which(
-      min_part <= min_dist & min_part < nchar(tomatch) - 1)
-    ranked_matches[inds_to_rep, ] <-  partial_matches[inds_to_rep, ]      
-    min_dist[inds_to_rep] <- min_part[inds_to_rep]
-    
-    match_df <- data.frame(list(names_tomatch = tomatch, min = min_dist, ranked_matches))
-  }
+  
+    ## make df
+    df <- data.frame(list(names_tomatch = tomatch, 
+                          fixed_best = ranked_matches[, 1], min_fixed = min_dist,
+                          partial_best = partial_matches[, 1], min_partial = min_part, 
+                          ranked_matches))  }
   return(match_df)
 }
 
