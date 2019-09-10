193f65e1bfcf536ca0f74fa3aad54dc0a05a05b8
Malavika Rajeev <mrajeev08@gmail.com>
2019-09-10 15:10:07 -0400

    removing catchment assigning
    now for generating spatial covariates

--- R/01_generate_gis/04_assign_catchments.R
+++ R/01_generate_gis/04_assign_catchments.R
@@ -5,6 +5,25 @@
 ##' Author: Malavika Rajeev 
 ####################################################################################################
 
+##' Read in files 
+##' ------------------------------------------------------------------------------------------------
+##' 
+##' Libraries/source scripts 
+##' ------------------------------------------------------------------------------------------------
+##' 
+##' Extract Pop 1x1 km
+##' ------------------------------------------------------------------------------------------------
+##' 
+##' Assign catchments and travel times (weighted/unweighted and masked/unmasked)
+##' ------------------------------------------------------------------------------------------------
+##' 
+##' Get distance to closest CTAR
+##' ------------------------------------------------------------------------------------------------
+##' 
+##' Write out the shapefiles to processed/shapefiles/
+##' ------------------------------------------------------------------------------------------------
+
+
 ## Set-up
 rm(list = ls())
 mada_districts <- readOGR("output/shapefiles/districts.shp")
@@ -22,46 +41,5 @@ catchmat_dist_unmasked <- find.bydate(path = "output/", patt = "catchmat_distric
 catchmat_comm_masked <- find.bydate(path = "output/", patt = "catchmat_commune_masked", ind = 4, rank = 1)
 catchmat_comm_unmasked <- find.bydate(path = "output/", patt = "catchmat_commune_unmasked", ind = 4, rank = 1)
 
-##' Get catchments
-dist_catch_unmasked <- get.catchments(catchmat = catchmat_dist_unmasked, shape = mada_districts, 
-                                      place_names = mada_districts$id_2, point_names = gps_locs$CTAR,
-                                      type = "unmasked", admin = "district")
 
-dist_catch_masked <- get.catchments(catchmat = catchmat_dist_masked, shape = mada_districts, 
-                                    place_names = mada_districts$id_2, 
-                                    point_names = gps_locs$CTAR,
-                                    type = "masked", admin = "district")
-
-comm_catch_unmasked <- get.catchments(catchmat = catchmat_comm_unmasked, shape = mada_communes, 
-                                      place_names = mada_communes$id_3,
-                                      point_names = gps_locs$CTAR,
-                                      type = "unmasked", admin = "commune")
-
-comm_catch_masked <- get.catchments(catchmat = catchmat_comm_masked, shape = mada_communes, 
-                                    place_names = mada_communes$id_3, 
-                                    point_names = gps_locs$CTAR,
-                                    type = "masked", admin = "commune")
-
-## Checks
-# library(dplyr)
-# gg_communes <- fortify(mada_communes, region = "id_3")
-# gg_communes %>%
-#   mutate(id = as.numeric(id)) %>%
-#   left_join(mada_communes@data, by = c("id" = "id_3")) %>%
-#   mutate(id = as.factor(as.character(id))) %>%
-#   left_join(comm_catch_masked, by = c("id" = "admin")) -> gg_communes
-# ggplot() +
-#   geom_polygon(data = gg_communes, aes(x = long, y = lat, group = id, fill = ttime)) +
-#   scale_fill_gradient(low = "white", high = "red")
-# 
-# gg_communes <- fortify(mada_communes, region = "id_3")
-# gg_communes %>%
-#   mutate(id = as.numeric(id)) %>%
-#   left_join(mada_communes@data, by = c("id" = "id_3")) %>%
-#   mutate(id = as.factor(as.character(id))) %>%
-#   left_join(comm_catch_unmasked, by = c("id" = "admin")) -> gg_communes
-# ggplot() +
-#   geom_polygon(data = gg_communes, aes(x = long, y = lat, group = id, fill = ttime)) +
-#   scale_fill_gradient(low = "white", high = "red") +
-#   geom_point(data = gps_locs, aes(x = Y_COORD, y = X_COORD))
 
