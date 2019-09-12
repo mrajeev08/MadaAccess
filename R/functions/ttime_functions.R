8875028ff44ff3b5258e42aceca48b8bc02045e7
Malavika Rajeev <mrajeev08@gmail.com>
2019-09-12 16:42:55 -0400

    Adding in doc and removed scenario functions

--- R/functions/ttime_functions.R
+++ R/functions/ttime_functions.R
@@ -1,10 +1,33 @@
-## Functions for working with friction surface + travel time layer
-## Malavika Rajeev 2018
+####################################################################################################
+##' Travel time functions 
+##' Details: Various functions for getting travel times, catchments, etc. 
+##' Author: Malavika Rajeev 
+####################################################################################################
 
 
+##' 1. Getting travel times 
+##' ------------------------------------------------------------------------------------------------
+#' Get minimum travel times
+#' \code[get.travel.times] calculates the minimum travel times for an input raster to an input set of 
+#' GPS points. 
+#' This function uses the friction surface from the Malaria Atlas Project. Script adapted
+#' from https://map.ox.ac.uk/research-project/accessibility_to_cities/. Uses least-cost algorithms
+#' from the gdistance package.
+#' @param friction raster, the friction surface downloaded from MAP website
+#' @param shapefile shapefile, to mask the friction surface to
+#' @param coords matrix of two columns of x(longitude) and y (latitude) points to 
+#'   input to calculate the least-cost distance (here travel times)
+#' @param trans_matrix_exists logical, if TRUE then looks for file as specified by filename_trans
+#'   if FALSE then creates the transition matrix using function transition from gdistance package
+#' @param filename_trans character vector, the path to which the transition matrix should either be 
+#'   read from or written to
+#' @return raster at same resolution as the input friction surface and cropped to the shapefile with 
+#'   the minimum travel time estimate as the values
+#' @section Dependencies:
+#'  Packages: gdistance, raster 
 # 1. Getting travel time raster layer from set of points -----------------------------------------
 ## Function for getting travel times given points 
-## Adapted from https://map.ox.ac.uk/research-project/accessibility_to_cities/
+## Adapted from 
 
 get.travel.times <- function(friction, shapefile, coords, trans_matrix_exists = TRUE, 
                              filename_trans){
@@ -46,8 +69,16 @@ get.travel.times <- function(friction, shapefile, coords, trans_matrix_exists = 
 }
 
 
-# 2. Get catchments for each admin unit (district or commune) ------------------------------------
-### Function for getting catchment matrix
+##' 2. Getting minimum travel times for each point to each admin unit 
+##' ------------------------------------------------------------------------------------------------
+#' Title
+#' Description
+#' Details
+#' @param Paramters
+#' @return Returned
+#' @section Dependencies:
+#'     List dependencies here, i.e. packages and other functions
+
 ## Admin level average or weighted average by pop for each clinic
 
 get.catchmat <- function(point_mat, fric, shape, admin = "district", pop_rast, 
@@ -87,133 +118,3 @@ get.catchmat <- function(point_mat, fric, shape, admin = "district", pop_rast,
   return(catchmat)
 }
 
-
-# 3. Add ARMC for scenario analysis --------------------------------------------------------------
-## parallelized function to add ARMC sequentially
-## updating base proportion at each step to eliminate ones that don't reduce that threshold further
-## (i.e. should remove clustered clinic locations)
-add.armc <- function(current_ARMC, candidate_ARMC, prop_pop, threshold, delta_tt_min = 1e-4,
-                     steps = 1000, base_prop,
-                     friction, shapefile, filename_trans) {
-  ## to test
-  # current_ARMC = gps_locs; candidate_ARMC = csbs[seq(1, 1000, by = 100), ];
-  # threshold = 3; delta_tt_min = 1e-20;
-  # steps = 10;
-  # friction = friction_unmasked; shapefile = mada_district;
-  # filename_trans = "output/trans_gc_unmasked.rds";
-  
-  ## For the number of possible clinics you could add
-  prop_under <- rep(NA, ncol(candidate_ARMC))
-  cands <- nrow(candidate_ARMC)
-  
-  ## empty data frame to bind to
-  new_ARMC <- data.frame(CTAR = rep(NA, cands), X_COORD = rep(NA, cands), Y_COORD = rep(NA, cands))
-  
-  print("starting iterative adding of ARMC")
-  print(Sys.time())
-  
-  for (i in 1:steps) {
-    if (nrow(candidate_ARMC) > 0) {
-      ## Iterate through and find the one that when you add it reduces travel times the most
-      ranked_coords <- foreach(j = 1:nrow(candidate_ARMC),
-                               .packages = c('raster', 'rgdal', 'sp', 'gdistance'),
-                               .errorhandling = 'stop',
-                               .export = 'get.travel.times', 
-                               .combine = c
-      ) %dopar% {
-        point_mat <- rbind(current_ARMC, candidate_ARMC[j, ])
-        point_mat <- as.matrix(cbind(point_mat$Y_COORD, point_mat$X_COORD)) # matrix of long and lat
-        ttimes <- get.travel.times(friction, shapefile, coords = point_mat, trans_matrix_exists = TRUE, 
-                                   filename_trans)
-        
-        ## proportion of pop living greater than threshold hrs away from CTAR (defaults to 3 hrs)
-        sum(prop_pop[ttimes >= 60*threshold], na.rm = TRUE) 
-      }
-      
-      ## save the data for the one that gets added
-      if(min(ranked_coords, na.rm = TRUE)[1] != Inf){
-        ## rank the one that minimizes this and add to current armc
-        current_ARMC <- rbind(current_ARMC, 
-                              candidate_ARMC[which(ranked_coords == min(ranked_coords, 
-                                                                        na.rm = TRUE)[1]), ])
-        write.csv(current_ARMC, "output/temp_ARMC.csv")
-        
-        ## add prop_under to the top ranked at the row i + 1 (because of place holder NA row)
-        prop_under[i] <- min(ranked_coords, na.rm = TRUE)[1]
-        write.csv(prop_under, "output/temp_prop_under.csv")
-        
-        ## add to new armc list
-        new_ARMC[i, ] <- candidate_ARMC[which(ranked_coords == min(ranked_coords, na.rm = TRUE)[1]), ]
-      }
-      
-      ## remove top ranked and also will result in no rows if ranked_coords is all NA
-      candidate_ARMC <- candidate_ARMC[-which(ranked_coords == min(ranked_coords, na.rm = TRUE)[1]), ]
-      
-      ## remove all which don't reduce above a certain threshold
-      candidate_ARMC <- candidate_ARMC[-which(base_prop - ranked_coords <= delta_tt_min), ] 
-      
-      ## output to continue analysis in case of cluster timeout
-      write.csv(candidate_ARMC, "output/temp_candidates.csv")
-      
-      ## new baseline proportion to diff against 
-      base_prop <- prop_under[i] 
-      
-      ## check for each loop to keep track of time
-      print(paste(i, "ARMC added"))
-      print(Sys.time())
-      print(paste(nrow(candidate_ARMC), "remaining"))
-    }
-    else{
-      next()
-    }
-  }
-  # new_ARMC$prop_under <- prop_under ## This throws errors due to discrepancies so avoid this for now
-  return(list(current_ARMC = current_ARMC, new_ARMC = new_ARMC, prop_under = prop_under))
-}
-
-
-# 4. Get travel times at admin level for scenario analysis ------------------------------------
-## Calculating travel times at whatever admin level for the scenario analysis
-run.scenario <- function(current_ARMC, new_ARMC, friction, shape, pop_rast, pop_pol, 
-                         admin = "district", weighted = TRUE, filename_trans) {
-  ## to test
-  # current_ARMC = gps_locs; candidate_ARMC = csbs[seq(1, 1000, by = 100), ];
-  # friction = friction_unmasked; shape = mada_district;  
-  # filename_trans = "output/trans_gc_unmasked.rds";
-  
-  print("starting ttime calcs for addtl ARMC")
-  print(Sys.time())
-  
-  ## getting catchments
-  ttime_mat <- foreach(j = 1:nrow(new_ARMC),
-                       .packages = c('raster', 'rgdal', 'sp', 'gdistance'),
-                       .errorhandling = 'stop',
-                       .export = 'get.travel.times',
-                       .combine = "cbind"
-  ) %dopar% {
-    
-    point_mat <- rbind(current_ARMC, new_ARMC[1:j, ])
-    point_mat <- as.matrix(cbind(point_mat$Y_COORD, point_mat$X_COORD)) # matrix of long and lat
-    ttimes <- get.travel.times(friction, shapefile = shape, coords = point_mat, trans_matrix_exists = TRUE, 
-                               filename_trans)
-    
-    if (weighted == TRUE){
-      ## add 1e-6 to 0 ttimes
-      ttimes[ttimes == 0] <- 1e-6
-      weighted_ttimes <- ttimes*pop_rast
-      names(weighted_ttimes) <- "w_ttimes"
-      out <- raster::extract(weighted_ttimes, shape, fun = sum, 
-                             na.rm = TRUE, df = TRUE, sp = TRUE, small = TRUE)
-      out$ttimes <- out$w_ttimes/pop_pol
-    } else {
-      names(ttimes) <- "ttimes"
-      out <- raster::extract(ttimes, shape, fun = mean, 
-                             na.rm = TRUE, df = TRUE, sp = TRUE, small = TRUE)
-    }
-    out$ttimes 
-  }
-  
-  write.csv(ttime_mat, paste0("output/", admin, "_", "scenario", "_", 
-                              format(Sys.time(), "%Y%m%d_%H%M%S"), ".csv"))
-  return(ttime_mat)
-}
