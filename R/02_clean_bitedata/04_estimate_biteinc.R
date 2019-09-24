ba44ea6f7f95cde0c3bf1a82f735e22ffa8af2c4
Malavika Rajeev <mrajeev08@gmail.com>
2019-09-24 16:55:47 -0400

    updating thresholds

--- R/02_clean_bitedata/04_estimate_biteinc.R
+++ R/02_clean_bitedata/04_estimate_biteinc.R
@@ -18,7 +18,7 @@ mada_communes <- readOGR("data/processed/shapefiles/mada_communes.shp")
 mada_districts <- readOGR("data/processed/shapefiles/mada_districts.shp")
 source("R/functions/data_functions.R")
 
-##' 1. National data (at district level)
+##' 1. Getting reporting and contacts
 ##' ------------------------------------------------------------------------------------------------
 bitedata %>%
   filter(source != "Moramanga", year(date_reported) > 2013, 
@@ -44,11 +44,66 @@ throughput %>%
          year = year(date_reported)) -> throughput
 
 
+## Fig S1A days to include based on a given threshold
 ## yearly reporting
 throughput %>%
   group_by(year, id_ctar) %>%
   summarize(reporting = sum(include/365)) -> reporting
 
+throughput %>%
+  left_join(reporting) %>%
+  mutate(n = ifelse(include == 1, no_patients, NA)) -> plot_reporting
+
+p <- ggplot(data = plot_reporting, aes(x = date_reported, y = reorder(id_ctar, reporting))) + 
+  geom_tile(aes(fill = n)) +
+  scale_fill_gradientn(colours = c("white", "purple", "black"), values = c(0, 0.1, 1),
+                       na.value = alpha("lightgrey", 1), name = "Number of \n patients") +
+  xlim(ymd("2014-01-01"), ymd("2017-12-31")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  xlab("Year") +
+  ylab("ARMC") +
+  labs(tag = "A") +
+  theme(panel.background = element_blank())
+
+## FigS1B range of reporting ests based on thresholds
+col_vals <- ctar_metadata$fill
+names(col_vals) <- ctar_metadata$id_ctar
+
+throughput %>%
+  group_by(id_ctar) %>%
+  arrange(date_reported, .by_group = TRUE) %>%
+  mutate(include_5 = rle.days(no_patients, threshold = 15), 
+         include_15 = rle.days(no_patients, threshold = 5),
+         include_30 = rle.days(no_patients, threshold = 30),
+         mean_throughput = mean(no_patients[include == 1]),
+         sd_throughput = sd(no_patients[include == 1]),
+         estimated_contact = ifelse(no_patients >= mean_throughput + 3*sd_throughput, 
+                                    1, 0),
+         year = year(date_reported)) -> throughput
+
+
+## Fig S1A days to include based on a given threshold
+## yearly reporting
+throughput %>%
+  group_by(year, id_ctar) %>%
+  summarize(reporting = sum(include/365)) -> reporting
+n <- clinic_reporting %>%
+  mutate(prop15 = prop, prop5 = get.days(doses_wide = doses_wide, threshold = 5)[[1]]$prop,
+         prop30 = get.days(doses_wide = doses_wide, threshold = 30)[[1]]$prop) %>%
+  ggplot(., aes(x = reorder(ctar, prop), color = ctar)) +
+  geom_hline(yintercept = 0.25, color = "darkgrey", alpha = 0.5) +
+  geom_boxplot(aes(ymin = prop30, lower = prop15, middle = prop15, upper = prop15, ymax = prop5),
+               stat = "identity") +
+  scale_color_manual(values = col_vals) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
+        legend.position = "none") +
+  xlab("ARMC") +
+  ylab("Proportion of days included") +
+  coord_flip() +
+  labs(tag = "B")
+
+##' Output estimates of bite incidence with covariates 
+##' ------------------------------------------------------------------------------------------------
 bitedata %>%
   filter(source != "Moramanga", year(date_reported) > 2013, 
          year(date_reported) < 2018, !is.na(distcode), !is.na(id_ctar), 
@@ -63,6 +118,7 @@ bites %>%
   group_by(year, distcode, id_ctar) %>%
   summarize(bites = n()) %>%
   left_join(reporting) %>% 
+  ## TO DO: need to filter out any catchments with reporting less than that too!
   ## filter any years with reporting < 25%
   filter(reporting > 0.25) %>%
   ## correct for reporting
@@ -75,6 +131,8 @@ bites %>%
 ## Then join with covars and catchment variables (filter any to exclude)
 bite_incidence %>%
   left_join(mada_districts@data, by = c("distcode" = "distcod")) -> ests
+
+## then output
 
 ## filter out excluded catchments
 
