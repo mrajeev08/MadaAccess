158011ca3c428d79b9c5ca7f2f31ca9247f77bbc
Malavika Rajeev <mrajeev08@gmail.com>
2019-09-24 16:48:14 -0400

    rename script folders

---
+++ R/03_bitemodels/run_bitemods.R
@@ -0,0 +1,5 @@
+####################################################################################################
+##' Run models of bite incidence using spatial covariates 
+##' Details: Models include travel times and distance as access metrics, in addition to population 
+##' Author: Malavika Rajeev 
+####################################################################################################
