29301c16f2388f88ff80c4321cc5df854f63b272
Malavika Rajeev <mrajeev08@gmail.com>
2019-09-24 16:57:05 -0400

    added function rle.days

--- R/functions/data_functions.R
+++ R/functions/data_functions.R
@@ -1,6 +1,29 @@
-############ DATA FUNCTIONS ########################################################################
-## Malavika Rajeev
-## March 2019
+####################################################################################################
+##' Functions for data wrangling/processing 
+##' Details: for getting days to include, reporting, etc. 
+##' Author: Malavika Rajeev 
+####################################################################################################
+
+##' Function for getting days to include via RLEs 
+##' ------------------------------------------------------------------------------------------------
+rle.days <- function(vec, threshold = 10) {
+  
+  rle(vec) %>%
+    unclass() %>%
+    as.data.frame() %>%
+    mutate(end = cumsum(lengths),
+    start = c(1, lag(end)[-1] + 1),
+    include = ifelse(values == 0 &
+    lengths >= threshold, 0, 1)) -> rles
+  
+  include <- rep(NA, length(vec))
+  
+  for(i in 1:nrow(rles)) {
+    include[rles$start[i]:rles$end[i]] <- rles$include[i]
+  }
+  
+  return(include)
+}
 
 ## Get days ------------------------------------------------------------------------------------
 get.days <- function(dmat = dose_mat, doses_wide, threshold = 10) {
