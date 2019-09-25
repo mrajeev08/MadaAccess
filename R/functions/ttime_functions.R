8c7b322a7c3ec7dd75cd93917c54c7e59ed78bc6
Malavika Rajeev <mrajeev08@gmail.com>
2019-09-25 14:35:27 -0400

    removed redundant code for the coordinate matrix

--- R/functions/ttime_functions.R
+++ R/functions/ttime_functions.R
@@ -32,9 +32,8 @@ get.travel.times <- function(friction, shapefile, coords, trans_matrix_exists = 
   ## crop friction surface to shapefile
   friction <- crop(friction, shapefile)
   
-  ## calculating travel times
   ## Fetch the number of points
-  n_points <- dim(coords)[1]
+  n_points <- nrow(coords)
   
   ## Make the graph and the geocorrected version of the graph (or read in the latter).
   if (trans_matrix_exists == TRUE) {
@@ -48,14 +47,8 @@ get.travel.times <- function(friction, shapefile, coords, trans_matrix_exists = 
     saveRDS(trans_gc, filename_trans)
   }
   
-  ## Convert the points into a matrix
-  xy_df <- data.frame()
-  xy_df[1:n_points, 1] <- coords[, 1]
-  xy_df[1:n_points, 2] <- coords[, 2]
-  xy_matrix <- as.matrix(xy_df)
-  
   ## Run the accumulated cost algorithm to make the final output map. This can be quite slow (potentially hours).
-  travel_times <- accCost(trans_gc, xy_matrix)
+  travel_times <- accCost(trans_gc, coords)
   
   # ## Clip to Mada
   # travel_times <- crop(travel_times, shapefile)
