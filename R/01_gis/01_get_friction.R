700e7c2ad7397d60488616e2aa32047726213ad4
Malavika Rajeev <mrajeev08@gmail.com>
2019-09-25 14:35:01 -0400

    got rid of extraneous ctar point formatting
    no longer outputting

--- R/01_gis/01_get_friction.R
+++ R/01_gis/01_get_friction.R
@@ -13,12 +13,12 @@ library(rgdal)
 library(gdistance)
 
 ##' Shapefile for masking
-mada_communes <- readOGR("data/raw/shapefiles/communes/mdg_admbnda_adm3_BNGRC_OCHA_20181031.shp")
+mada_districts <- readOGR("data/raw/shapefiles/districts/mdg_admbnda_adm2_BNGRC_OCHA_20181031.shp")
 
 ##' Masked friction surface
 friction_masked <- getRaster(
     surface = "A global friction surface enumerating land-based travel speed for a nominal year 2015",
-    shp = mada_communes)
+    shp = mada_districts)
 plot(friction_masked) ## test
 writeRaster(friction_masked, "data/processed/rasters/friction_mada_masked.tif", overwrite = TRUE)
 
@@ -32,7 +32,7 @@ saveRDS(trans_gc, "data/processed/rasters/trans_gc_masked.rds")
 ## takes a long time (~ 15 minutes)
 friction_world <- malariaAtlas::getRaster(
   surface = "A global friction surface enumerating land-based travel speed for a nominal year 2015")
-friction_unmasked <- crop(friction_world, mada_communes)
+friction_unmasked <- crop(friction_world, mada_districts)
 plot(friction_unmasked) ## test
 writeRaster(friction_unmasked, "data/processed/rasters/friction_mada_unmasked.tif", overwrite = TRUE)
 
@@ -41,13 +41,3 @@ writeRaster(friction_unmasked, "data/processed/rasters/friction_mada_unmasked.ti
 trans <- transition(friction_unmasked, function(x) 1/mean(x), 8) # RAM intensive, can be very slow for large areas
 trans_gc <- geoCorrection(trans)
 saveRDS(trans_gc, "data/processed/rasters/trans_gc_unmasked.rds")
-
-##' Write out current CTAR point mat
-##' ------------------------------------------------------------------------------------------------
-##' Locations of CTAR (N = 31)
-gps_locs <- read.csv(file = "data/raw/ctar_metadata.csv")[, c(1, 3, 4)] ##' to do: change this so not indexing columns!
-names(gps_locs) <- c ("CTAR", "X_COORD", "Y_COORD")
-coordinates(gps_locs) <- ~ Y_COORD + X_COORD
-proj4string(gps_locs) <- proj4string(mada_communes)
-point_mat <- as.matrix(gps_locs@coords)
-write.csv(point_mat, "data/processed/point_mat_CTAR.csv", row.names = FALSE)
