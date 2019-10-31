b6395478bb013e4dc87d9cb1174f1820745ee25a
Malavika Rajeev <mrajeev08@gmail.com>
2019-10-31 12:37:46 -0400

    different ggplot options

--- R/04_burden/01_get_burden.R
+++ R/04_burden/01_get_burden.R
@@ -7,6 +7,7 @@
 source("R/02_bitedata/03_estimate_biteinc.R") # either source this or output bite data
 # probs want to output bite data in order to pull into other things
 source("R/functions/predict_bites.R")
+source("R/functions/utils.R")
 
 ## libraries
 library(foreach)
@@ -56,7 +57,7 @@ preds_burden <-
     predict.deaths(access = covar_df[, as.character(i$covar_name)], ctar_in = covar_df$ctar_in_district, 
                    pop = covar_df$pop, catch = catches, names = covar_df$names, 
                    group_name = covar_df$group_name, beta_access = i$beta_access, 
-                   beta_ctar = i$beta_ctar, beta_0 = i$beta_0, beta_pop = i$beta_pop, 
+                   beta_ctar = 0, beta_0 = i$beta_0, beta_pop = i$beta_pop, 
                    sigma_0 = i$sigma_0, 
                    known_alphas = NA, 
                    covar_name = i$covar_name, pop_predict = i$pop_predict, intercept = i$intercept,
@@ -81,34 +82,92 @@ preds_burden %>%
   summarize_at(vars(starts_with("p_rabid"), starts_with("reporting"), starts_with("access")), mean, na.rm = TRUE) %>%
   left_join(grouped_deaths) -> grouped_deaths
 
+to_plot <- filter(grouped_deaths, intercept == "random", pop_predict == "flatPop", 
+                  scale == "Commune")
+to_plot$scale_of_data <- "District"
+comm_to_plot <- filter(preds_burden, intercept == "random", pop_predict == "flatPop", 
+                       scale == "Commune")
+comm_to_plot$scale_of_data <- "Commune"
+burden_to_plot <- bind_rows(comm_to_plot, to_plot)
 
-ggplot(data = filter(grouped_deaths, intercept == "fixed"), 
-       aes(x = reorder_within(group_name, access, covar_name), y = deaths_mean/pop*1e5, 
-           color = interaction(scale, data_source), shape = ctar_bump)) +
-  geom_point(alpha = 0.5, size = 2) +
-  scale_color_manual(values = c("darkred", "#cc7722", "#004b49"), name = "Scale") +
-  facet_grid(pop_predict ~ covar_name, scales = "free_x", drop = TRUE) +
-  expand_limits(y = 0) +
-  xlab("Districts (ordered by decreasing access") +
+ggplot() +
+  geom_point(data = burden_to_plot, 
+             aes(x = reorder_within(group_name, access, covar_name), y = deaths_mean/pop*1e5, 
+                 fill = access, shape = scale_of_data, alpha = scale_of_data,
+                 size = scale_of_data, color = scale_of_data)) +
+  scale_fill_gradient(low = alpha("#fdebef", 1), high = alpha("#e52b50", 1), 
+                      name = "Travel times (hrs)")  +
+  scale_shape_manual(values = c(22, 23)) +
+  scale_alpha_manual(values = c(0.75, 1)) +
+  scale_size_manual(values = c(1.5, 2)) +
+  scale_color_manual(values = c("grey", "black")) +
+  xlab("Districts (ordered by decreasing access)") +
   ylab("Predicted incidence of deaths") +
   labs(tag = "A") +
-  theme(axis.text.x = element_blank())
+  geom_hline(aes(yintercept = sum(comm_to_plot$deaths_mean)/sum(comm_to_plot$pop)*1e5), linetype = 2, 
+             color = "darkgrey") +
+  theme(axis.text.y = element_blank(), panel.grid.minor = element_blank(), 
+        panel.grid.major.x = element_blank()) +
+  coord_flip()
 
-ggplot(data = filter(grouped_deaths, intercept == "random"), 
-       aes(x = access, y = deaths_mean/pop*1e5, 
-           color = interaction(scale, data_source), shape = ctar_bump)) +
-  geom_point(alpha = 0.5, size = 2) +
-  scale_color_manual(values = c("darkred", "#cc7722", "#004b49"), name = "Scale") +
-  facet_grid(pop_predict ~ covar_name, scales = "free_x", drop = TRUE) +
-  expand_limits(y = 0) +
-  xlab("Access metric") +
+ggplot() +
+  geom_point(data = comm_to_plot, 
+             aes(x = reorder_within(group_name, access, covar_name), y = deaths_mean/pop*1e5,
+                 fill = access), shape = 22, color = alpha("black", 0.1)) +
+  geom_point(data = to_plot, 
+             aes(x = reorder_within(group_name, access, covar_name), y = deaths_mean/pop*1e5,
+                 fill = access), shape = 23, color = "grey", size = 2, stroke = 1.1) +
+  scale_fill_gradient(low = alpha("#fdebef", 1), high = alpha("#e52b50", 1), 
+                      name = "Travel times (hrs)") +
+  xlab("Districts (ordered by \n decreasing access)") +
   ylab("Predicted incidence of deaths") +
-  labs(tag = "B") 
+  labs(tag = "A") +
+  geom_hline(aes(yintercept = sum(to_plot$deaths_mean)/sum(to_plot$pop)*1e5), linetype = 2, 
+             color = "darkgrey") +
+  theme(axis.text.y = element_blank(), panel.grid.minor = element_blank(), 
+        panel.grid.major.x = element_blank()) +
+  coord_flip()
+
+M4.A <- ggplot() +
+  geom_point(data = comm_to_plot, 
+             aes(x = reorder_within(group_name, access, covar_name), y = deaths_mean/pop*1e5,
+             fill = access), shape = 22, color = alpha("grey", 0.5)) +
+  geom_point(data = to_plot, 
+             aes(x = reorder_within(group_name, access, covar_name), y = deaths_mean/pop*1e5),
+             color = "#6a274c", shape = 22, size = 2, stroke = 1.1) +
+  scale_fill_gradient(low = alpha("#fdebef", 1), high = alpha("#e52b50", 1), 
+                      name = "Travel times (hrs)") +
+  xlab("Districts (ordered by \n decreasing access)") +
+  ylab("Predicted incidence of deaths") +
+  labs(tag = "A") +
+  geom_hline(aes(yintercept = sum(to_plot$deaths_mean)/sum(to_plot$pop)*1e5), linetype = 2, 
+             color = "darkgrey") +
+  theme(axis.text.y = element_blank(), panel.grid.minor = element_blank(), 
+        panel.grid.major.x = element_blank()) +
+  coord_flip()
+
+M4.B <- ggplot() +
+  geom_point(data = comm_to_plot, 
+             aes(x = reorder_within(group_name, access, covar_name), y = reporting_mean,
+                 fill = access), shape = 22, color = alpha("grey", 0.5)) +
+  geom_point(data = to_plot, 
+             aes(x = reorder_within(group_name, access, covar_name), y = reporting_mean),
+             color = "#00a86b", shape = 22, size = 2, stroke = 1.1) +
+  scale_fill_gradient(low = alpha("#f9fbf9", 1), high = alpha("#2e5133", 1), 
+                      name = "Travel times (hrs)") +
+  xlab("Districts (ordered by \n decreasing access)") +
+  ylab("Average reporting") +
+  labs(tag = "B") +
+  geom_hline(aes(yintercept = mean(to_plot$reporting_mean)), linetype = 2, 
+             color = "darkgrey") +
+  theme(axis.text.y = element_blank(), panel.grid.minor = element_blank(), 
+        panel.grid.major.x = element_blank()) +
+  coord_flip()
+
+figM4 <- M4.A / M4.B
+ggsave("figs/M4.pdf", figM4, device = "pdf", height = 12, width = 8)
 
 grouped_deaths %>%
   group_by(data_source, covar_name, scale, pop_predict, ctar_bump, 
            summed, intercept) %>% 
   summarize_at(vars(bites_mean:averted_lower05, pop), sum, na.rm = TRUE) -> national_deaths
-
-##' Scenario 
-##' ------------------------------------------------------------------------------------------------
