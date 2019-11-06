87b0d903592fd13d9b56538361cc61c3ad59fc2a
Malavika Rajeev <mrajeev08@gmail.com>
2019-11-06 08:46:05 -0500

    changed to new way at 1x1 km grid cell

--- R/05_scenario/01_run_candidates.R
+++ R/05_scenario/01_run_candidates.R
@@ -1,124 +1,83 @@
 ####################################################################################################
-##' Step 1: Generating catchment matrix for all possible additional clinics
-##' Details: Getting travel times and distance to each clinic for each admin unit 
-##'   Recommended to run in parallel to limit compute time
-##'   On the Della cluster at Princeton with 32 cores, it takes approximately N minutes
-##' Author: Malavika Rajeev 
+##' Testing catchment scripts
+##' Details: Getting travel time estimates and catchments for all clinics
+##'   Code must be run in parallel
+##'   On the Della cluster at Princeton with NN cores, it takes approximately NN minutes
+##' Author: Malavika Rajeev
 ####################################################################################################
 
-##' Set-up 
+##' Set up
 ##' ------------------------------------------------------------------------------------------------
-##' Cluster
+Sys.time()
 rm(list = ls())
-# args <- commandArgs(trailingOnly = TRUE)
-# cores <- as.integer(args[9])
-library(doMPI)
-cl <- startMPIcluster()
-clusterSize(cl) # this just tells you how many you've got
-registerDoMPI(cl)
 
 ##' Libraries
 library(rgdal)
 library(raster)
-library(gdistance)
-library(doRNG)
 library(foreach)
+library(tidyverse)
+library(gdistance)
 library(iterators)
-library(geosphere)
-library(dplyr)
-select <- dplyr::select
+library(doParallel)
+library(data.table)
+
+##' Source
+source("R/functions/utils.R")
 source("R/functions/access_functions.R")
 
-##' Shapefiles and CTAR point matrix
+##' Load in GIS files 
 mada_communes <- readOGR("data/processed/shapefiles/mada_communes.shp")
 mada_districts <- readOGR("data/processed/shapefiles/mada_districts.shp")
 pop1x1<- raster("data/processed/rasters/worldpop2015adj_mada_1x1km.tif")
 ctar_metadata <- read.csv("data/raw/ctar_metadata.csv")
-friction_mada_masked <- raster("data/processed/rasters/friction_mada_masked.tif")
-friction_mada_unmasked <- raster("data/processed/rasters/friction_mada_unmasked.tif")
+friction_masked <- raster("data/processed/rasters/friction_mada_masked.tif")
 
 ## candidate points
 csbs <- read.csv("data/raw/csbs.csv", stringsAsFactors = FALSE)
 csbs %>% 
   filter(type == "CSB2", genre_fs != "Priv", type_fs != "Health Post") %>%
   dplyr::select(CTAR = nom_fs, X_COORD = ycoor, Y_COORD = xcoor) -> csbs
-point_mat <- as.matrix(select(csbs, Y_COORD, X_COORD))
 
-##' Quick function for getting catchments accounting for Inf returns
-which.min.inf  <- function(x) {
-  if(min(x) == Inf | is.na(min(x))) {
-    return(NA)
-  } else {
-    return(which.min(x))
-  }
+point_mat_candidates <- as.matrix(select(csbs, Y_COORD, X_COORD))
+candidate_ids <- 1:nrow(csbs) + 31 ## above the baseline 
+baseline_df <- fread("output/baseline.csv")
+
+## Do the candidates
+cl <- makeCluster(3)
+registerDoParallel(cl)
+
+system.time ({
+  foreach(points = iter(point_mat_candidates, by = "row"),
+          .packages = c("raster", "gdistance", "data.table")) %dopar% {
+            ttimes <- get.access(friction = friction_masked, shapefile = mada_districts,
+                                 coords = points, trans_matrix_exists = TRUE,
+                                 filename_trans = "data/processed/rasters/trans_gc_masked.rds",
+                                 metric = "ttimes")
+          } -> stacked_ttimes
+})
+
+stopCluster(cl) ## for doParallel
+## 6 seconds per point
+
+## stack it
+stacked_ttimes <- do.call("stack", stacked_ttimes)
+stacked_ttimes <- raster::as.matrix(stacked_ttimes)
+stacked_ttimes <- stacked_ttimes[!is.na(getValues(friction_masked)), ]
+
+## Filter out any that shift less than 0.01% of people over
+prop.lessthan <- function(x, prop_pop, base_metric, threshold) {
+  ## Sum of the proportion of the population changed weighted by how much changed below threshold
+  sum(prop_pop[which(x < base_metric & x > threshold)], na.rm = TRUE)
 }
 
-## District
-## Weighted travel times
-dist_mat_unmasked <- get.catchmat(point_mat = point_mat, fric = friction_mada_unmasked, 
-                                  point_names = csbs$CTAR, admin_names = mada_districts$distcode,
-                                  shape = mada_districts, pop_rast = pop1x1, 
-                                  pop_pol = mada_districts$pop, 
-                                  trans_mat = "data/processed/rasters/trans_gc_unmasked.rds",
-                                  weighted = TRUE, met = "ttimes")
-dist_mat_masked <- get.catchmat(point_mat = point_mat, fric = friction_mada_masked, 
-                                point_names = csbs$CTAR, admin_names = mada_districts$distcode,
-                                shape = mada_districts, pop_rast = pop1x1, 
-                                pop_pol = mada_districts$pop, 
-                                trans_mat = "data/processed/rasters/trans_gc_masked.rds",
-                                weighted = TRUE, met = "ttimes")
-dist_mat_masked[which(is.na(apply(dist_mat_masked, 1, which.min.inf))), ] <- 
-  dist_mat_unmasked[which(is.na(apply(dist_mat_masked, 1, which.min.inf))), ]
-write.csv(dist_mat_masked, "data/processed/catchmats/candidates_district_ttimes_weighted.csv")
+change_prop <- foreach(vals = iter(stacked_ttimes, by = "col"), 
+                       .combine = c) %do% {
+                         prop.lessthan(vals, prop_pop = prop_pop, base_metric = ttimes_weighted, 
+                                       threshold = 3*60)
+                       }
 
-## Weighted distance
-dist_mat_masked <- get.catchmat(point_mat = point_mat, fric = friction_mada_masked, 
-                                point_names = csbs$CTAR, admin_names = mada_districts$distcode,
-                                shape = mada_districts, pop_rast = pop1x1, 
-                                pop_pol = mada_districts$pop, 
-                                weighted = TRUE, met = "distance")
-write.csv(dist_mat_masked, "data/processed/catchmats/candidates_district_distance_weighted.csv")
+stacked_ttimes <- stacked_ttimes[, -which(change_prop < 0.0001)]
+candidate_ids <- candidate_ids[which(change_prop > 0.0001)]
 
-## Commune
-## Weighted travel times
-comm_mat_unmasked <- get.catchmat(point_mat = point_mat, fric = friction_mada_unmasked, 
-                                  point_names = csbs$CTAR, admin_names = mada_communes$ADM3_PCODE,
-                                  shape = mada_communes, pop_rast = pop1x1, 
-                                  trans_mat = "data/processed/rasters/trans_gc_unmasked.rds",
-                                  pop_pol = mada_communes$pop, weighted = TRUE,
-                                  met = "ttimes")
-comm_mat_masked <- get.catchmat(point_mat = point_mat, fric = friction_mada_masked, 
-                                shape = mada_communes, pop_rast = pop1x1, 
-                                point_names = csbs$CTAR, admin_names = mada_communes$ADM3_PCODE,
-                                trans_mat = "data/processed/rasters/trans_gc_masked.rds",
-                                pop_pol = mada_communes$pop, weighted = TRUE, 
-                                met = "ttimes")
-comm_mat_masked[which(is.na(apply(comm_mat_masked, 1, which.min.inf))), ] <- 
-  comm_mat_unmasked[which(is.na(apply(comm_mat_masked, 1, which.min.inf))), ]
-write.csv(comm_mat_masked, "data/processed/catchmats/candidates_commune_ttimes_weighted.csv")
-
-
-## Weighted distance
-comm_mat_masked <- get.catchmat(point_mat = point_mat, fric = friction_mada_masked, 
-                                shape = mada_communes, pop_rast = pop1x1, 
-                                point_names = csbs$CTAR, admin_names = mada_communes$ADM3_PCODE,
-                                pop_pol = mada_communes$pop, weighted = TRUE,
-                                met = "distance")
-write.csv(comm_mat_masked, "data/processed/catchmats/candidates_commune_distance_weighted.csv")
-
-##' Distance Centroid
-##' Communes
-mada_commune_coords <- coordinates(mada_communes)
-comm_distance_mat <- distm(mada_commune_coords, point_mat)/1000
-write.csv(comm_distance_mat, "data/processed/catchmats/candidates_commune_distance_centroid.csv")
-
-##' Districts
-mada_district_coords <- coordinates(mada_districts)
-dist_distance_mat <- distm(mada_district_coords, point_mat)/1000
-write.csv(dist_distance_mat, "data/processed/catchmats/candidates_district_distance_centroid.csv")
-
-##' Close out cluster
-closeCluster(cl)
-mpi.quit()
-print("Done :)")
-Sys.time()
+fwrite(stacked_ttimes, "output/candidate_matrix.csv")
+write.csv(candidate_ids, "output/candidate_ids.csv")
