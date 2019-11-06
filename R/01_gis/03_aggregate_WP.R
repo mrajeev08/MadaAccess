42eff4998a732199a86372d5ba10f9aff3612144
Malavika Rajeev <mrajeev08@gmail.com>
2019-11-06 15:09:47 -0500

    removing the extract pop

--- R/01_gis/03_aggregate_WP.R
+++ R/01_gis/03_aggregate_WP.R
@@ -60,22 +60,6 @@ writeRaster(pop1x1, "data/processed/rasters/worldpop2015adj_mada_1x1km.tif", ove
 ##' quick check
 sum(getValues(pop1x1), na.rm = TRUE)
 
-##' Extract pop to shapefiles and write out 
-##' ------------------------------------------------------------------------------------------------
-mada_communes$pop <- extract(pop1x1, mada_communes, fun = sum, small = TRUE, na.rm = TRUE)[, 1]
-mada_districts$pop <- extract(pop1x1, mada_districts, fun = sum, small = TRUE, na.rm = TRUE)[, 1]
-
-##' checks on extracted pop
-sum(mada_districts$pop, na.rm = TRUE)
-sum(mada_communes$pop, na.rm = TRUE)
-
-##' Write out the shapefiles to processed/shapefiles/ (overwrite)
-##' ------------------------------------------------------------------------------------------------
-writeOGR(mada_communes, dsn = "data/processed/shapefiles", layer = "mada_communes", 
-         driver = "ESRI Shapefile", overwrite_layer = TRUE)
-writeOGR(mada_districts, dsn = "data/processed/shapefiles", layer = "mada_districts", 
-         driver = "ESRI Shapefile", overwrite_layer = TRUE)
-
 ##' Close out cluster
 closeCluster(cl)
 mpi.quit()
