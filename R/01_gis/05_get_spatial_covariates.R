b22e9c289ac9e5cea6137748140a37a373162871
Malavika Rajeev <mrajeev08@gmail.com>
2019-11-06 15:09:58 -0500

    writing processed shapefiles new

--- R/01_gis/05_get_spatial_covariates.R
+++ R/01_gis/05_get_spatial_covariates.R
@@ -1,40 +1,53 @@
 ####################################################################################################
-##'Assigning catchment to each admin unit based on travel times
-##' Details: Uses data frames generated in step 04
-##'   Does not need to be run in parallel
+##' Generating shapefiles with covariates
 ##' Author: Malavika Rajeev 
 ####################################################################################################
-rm(list = ls())
+rm(list=ls())
 
-##' Libraries/source scripts 
-##' ------------------------------------------------------------------------------------------------
-library(geosphere)
+##' Libraries and packages
+library(rgeos)
+library(dplyr)
 library(raster)
 library(rgdal)
-library(tidyverse)
 library(data.table)
 
-##' Read in files 
-##' ------------------------------------------------------------------------------------------------
-mada_communes <- readOGR("data/processed/shapefiles/mada_communes.shp")
-mada_districts <- readOGR("data/processed/shapefiles/mada_districts.shp")
+##' Shapefiles
+mada_districts <- readOGR("data/raw/shapefiles/districts/mdg_admbnda_adm2_BNGRC_OCHA_20181031.shp")
+mada_communes <- readOGR("data/raw/shapefiles/communes/mdg_admbnda_adm3_BNGRC_OCHA_20181031.shp")
 ctar_metadata <- read.csv(file = "data/raw/ctar_metadata.csv")
-baseline_df <- fread("output/ttimes/baseline_grid.csv")
+
+##' Dissolving districts
+##' ------------------------------------------------------------------------------------------------
+##' because these are new districts and we do not have data to the arrondisement level for 
+##' Tana (i.e. Antananarivo Renivohitra)
+mada_districts$distcode <- substring(as.character(mada_districts$ADM2_PCODE), 1, 7)
+mada_communes$distcode <- substring(as.character(mada_communes$ADM2_PCODE), 1, 7)
+
+districts_dissolved <- gUnaryUnion(mada_districts, id = mada_districts$distcode)
+districts_df <- mada_districts@data[6:nrow(mada_districts@data), ]
+districts_df$ADM2_EN <- recode(districts_df$ADM2_EN, `6e Arrondissement` = "Antananarivo Renivohitra")
+row.names(districts_dissolved) <- as.character(1:nrow(districts_df))
+row.names(districts_df) <- as.character(1:nrow(districts_df))
+mada_districts <- SpatialPolygonsDataFrame(districts_dissolved, districts_df)
 
 ##' Get travel times and catchments at district and commune level
 ##' ------------------------------------------------------------------------------------------------
 district_df <- fread("output/ttimes/baseline_district.csv")
 district_df <- district_df[scenario == 31]
 district_df <- district_df[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRUE)], by = district_id]
-## Fix so that is base catch is 0, then NA (this means that Inf returned for all clinics)
-district_df[, weighted_times := ifelse(base_catches == 0, NA, weighted_times)]
+## Fix so that if base catch is 0, then NA (this means that Inf returned for all clinics)
+## Also match the clinic catchment name
+district_df[, c('weighted_times', 'catchment') := .(ifelse(base_catches == 0, NA, weighted_times),
+                                                    ifelse(base_catches == 0, NA, 
+                                                           ctar_metadata$CTAR[base_catches]))]
 
 ## Match district_id with row number in mada_districts@data
 setorder(district_df, district_id)
 if (nrow(district_df) == nrow(mada_districts@data)) {
   mada_districts$ttimes <- district_df$weighted_times
-  mada_districts$catch <- district_df$base_catches
+  mada_districts$catch <- district_df$catchment
   mada_districts$prop_pop <- district_df$prop_pop_catch
+  mada_districts$pop <- district_df$pop
 }
 
 ## Communes
@@ -42,17 +55,20 @@ commune_df <- fread("output/ttimes/baseline_commune.csv")
 commune_df <- commune_df[scenario == 31]
 commune_df <- commune_df[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRUE)], by = commune_id]
 ## Fix so that is base catch is 0, then NA (this means that Inf returned for all clinics)
-commune_df[, weighted_times := ifelse(base_catches == 0, NA, weighted_times)]
+commune_df[, c('weighted_times', 'catchment') := .(ifelse(base_catches == 0, NA, weighted_times),
+                                                    ifelse(base_catches == 0, NA, 
+                                                           ctar_metadata$CTAR[base_catches]))]
 
 ## Match commune_id with row number in mada_communes@data
 setorder(commune_df, commune_id)
 if (nrow(commune_df) == nrow(mada_communes@data)) {
   mada_communes$ttimes <- commune_df$weighted_times
-  mada_communes$catch <- commune_df$base_catches
+  mada_communes$catch <- commune_df$catchment
   mada_communes$prop_pop <- commune_df$prop_pop_catch
+  mada_communes$pop <- commune_df$pop
 }
 
-##' Get distance to closest CTAR based on centroids
+##' Get longitude and latitude
 ##' ------------------------------------------------------------------------------------------------
 ##' Districts
 mada_districts$long <- coordinates(mada_districts)[, 1]
@@ -76,8 +92,8 @@ mada_communes@data %>%
 
 ##' Write out the shapefiles to processed/shapefiles/ (overwrite)
 ##' ------------------------------------------------------------------------------------------------
-writeOGR(mada_communes, dsn = "data/output/shapefiles", layer = "mada_communes", 
+writeOGR(mada_communes, dsn = "data/processed/shapefiles", layer = "mada_communes", 
          driver = "ESRI Shapefile", overwrite_layer = TRUE)
-writeOGR(mada_districts, dsn = "data/output/shapefiles", layer = "mada_districts", 
+writeOGR(mada_districts, dsn = "data/processed/shapefiles", layer = "mada_districts", 
          driver = "ESRI Shapefile", overwrite_layer = TRUE)
 
