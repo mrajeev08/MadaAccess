46e0c16bef34aad8ce6b004f9d61e8255e5cad95
Malavika Rajeev <mrajeev08@gmail.com>
2019-11-06 15:10:16 -0500

    getting rid of the unmasked surfaces

--- R/01_gis/01_get_friction.R
+++ R/01_gis/01_get_friction.R
@@ -27,17 +27,3 @@ writeRaster(friction_masked, "data/processed/rasters/friction_mada_masked.tif", 
 trans <- transition(friction_masked, function(x) 1/mean(x), 8) # RAM intensive, can be very slow for large areas
 trans_gc <- geoCorrection(trans)
 saveRDS(trans_gc, "data/processed/rasters/trans_gc_masked.rds")
-
-##' Unmasked friction surface (still cropped to Mada)
-## takes a long time (~ 15 minutes)
-friction_world <- malariaAtlas::getRaster(
-  surface = "A global friction surface enumerating land-based travel speed for a nominal year 2015")
-friction_unmasked <- crop(friction_world, mada_districts)
-plot(friction_unmasked) ## test
-writeRaster(friction_unmasked, "data/processed/rasters/friction_mada_unmasked.tif", overwrite = TRUE)
-
-##' Unmasked transition surface
-##' Make and geocorrect the transition matrix (i.e., the graph)
-trans <- transition(friction_unmasked, function(x) 1/mean(x), 8) # RAM intensive, can be very slow for large areas
-trans_gc <- geoCorrection(trans)
-saveRDS(trans_gc, "data/processed/rasters/trans_gc_unmasked.rds")
