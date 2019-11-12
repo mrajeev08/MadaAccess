4c35c49021165bb4bcd9909b1c65ddfa9b3c42f4
Malavika Rajeev <mrajeev08@gmail.com>
2019-11-12 11:30:35 -0500

    running dist params and ttimes with comm catches

--- R/05_incremental/03_burden_incremental.R
+++ R/05_incremental/03_burden_incremental.R
@@ -9,14 +9,19 @@ rm(list=ls())
 library(data.table)
 library(tidyverse)
 library(rgdal)
+library(foreach)
+library(iterators)
+library(boot)
+library(ggridges)
 source("R/functions/predict_bites.R")
 
 ## District travel times
 ctar_metadata <- read.csv(file = "data/raw/ctar_metadata.csv")
-district_df <- fread("output/ttimes/incremental_district.csv")
 commune_df <- fread("output/ttimes/incremental_commune.csv")
+district_df <- fread("output/ttimes/incremental_district.csv")
 model_ests <- read.csv("output/mods/bitemod_results.csv")
 mada_districts <- readOGR("data/processed/shapefiles/mada_districts.shp")
+mada_communes <- readOGR("data/processed/shapefiles/mada_communes.shp")
 
 ## Get model means for commune and district models
 model_ests %>%
@@ -25,7 +30,7 @@ model_ests %>%
   tidyr::spread(key = params, value = Mean, fill = 0) %>%
   filter(pop_predict == "flatPop", data_source == "National", intercept == "random") -> model_means
 
-## Single catchment
+## Filter to a single catchment
 district_df <- district_df[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRUE)], 
                            by = .(district_id, scenario)]
 commune_df <- commune_df[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRUE)], 
@@ -35,77 +40,33 @@ commune_df <- commune_df[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRU
 district_df[, catch_numeric := as.numeric(as.factor(base_catches)), by = scenario]
 commune_df[, catch_numeric := as.numeric(as.factor(base_catches)), by = scenario]
 
-## Burden for district model
-predict.all(ttimes = district_df$weighted_times/60, 
-               pop = district_df$pop, catch = district_df$catch_numeric, 
-               names = district_df$district_id, 
-               beta_ttimes = model_means$beta_access[model_means$scale == "District"], 
-               beta_0 = model_means$beta_0[model_means$scale == "District"], beta_pop = 0, 
-               sigma_0 = model_means$sigma_0[model_means$scale == "District"], 
-               known_alphas = NA, pop_predict = "flatPop", intercept = "random",
-               data_source = "National", 
-               scale = "District",
-               trans = 1e5, known_catch = FALSE, 
-               p_rab_min = 0.2, p_rab_max = 0.6, rho_max = 0.98,
-               max_HDR = 25, min_HDR = 5, 
-               dog_rabies_inc = 0.01, human_exp_rate = 0.39, 
-               prob_death = 0.16, nsims = 1000, scenario = district_df$scenario) -> district_deaths
+## Match district ttimes to commune ids to get 
+mada_communes$match_id <- 1:nrow(mada_communes@data)
+commune_df$distcode <- mada_communes$distcode[match(commune_df$commune_id, mada_communes$match_id)]
+mada_districts$match_id <- 1:nrow(mada_districts@data)
+district_df$distcode <- mada_districts$distcode[match(district_df$district_id, mada_districts$match_id)]
+district_df_merge <- district_df[, 
+                                 c("distcode", "weighted_times", "scenario"), 
+                                 with = FALSE][, setnames(.SD, "weighted_times", "weighted_times_dist")]
+commune_df <- commune_df[district_df_merge, on = c("scenario", "distcode")]
+comm_by_scenario <- split(commune_df, by = "scenario")
 
-
-district_deaths <- data.table(district_deaths)
-district_grouped <- district_deaths[, .(deaths_mean = sum(deaths_mean)), by = scenario]
-ggplot(data = district_grouped, aes(x = scenario, y = deaths_mean)) + geom_line()
-
-
-## Single catchment
-district_max <- fread("output/ttimes/max_district.csv")
-district_max <- district_max[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRUE)], 
-                           by = .(district_id, scenario)]
-
-## Fix so that catches are numeric within the scenarios
-district_max[, catch_numeric := as.numeric(as.factor(max_catches))]
-
-predict.deaths(access = district_max$weighted_times/60, ctar_in = 0, 
-               pop = district_max$pop, catch = district_max$catch_numeric, 
-               names = district_max$district_id, 
-               group_name = district_max$district_id, 
-               beta_access = model_means$beta_access[model_means$scale == "District"], 
-               beta_ctar = 0, beta_0 = model_means$beta_0[model_means$scale == "District"], beta_pop = 0, 
-               sigma_0 = model_means$sigma_0[model_means$scale == "District"], 
-               known_alphas = NA, 
-               covar_name = "ttimes", pop_predict = "flatPop", intercept = "random",
-               summed = FALSE, ctar_bump = FALSE, data_source = "National", 
-               scale = "District",
-               trans = 1e5, known_catch = FALSE, 
-               p_rab_min = 0.2, p_rab_max = 0.6, rho_max = 0.98,
-               max_HDR = 25, min_HDR = 5, 
-               dog_rabies_inc = 0.01, human_exp_rate = 0.39, 
-               prob_death = 0.16, nsims = 1000, scenario = district_max$scenario) -> district_max
-sum(district_max$deaths_mean, na.rm = TRUE)
-
-## Burden for commune model
-predict.deaths(access = commune_df$weighted_times/60, ctar_in = 0, 
-               pop = commune_df$pop, catch = commune_df$catch_numeric, 
-               names = commune_df$commune_id, 
-               group_name = commune_df$commune_id, 
-               beta_access = model_means$beta_access[model_means$scale == "Commune"], 
-               beta_ctar = 0, beta_0 = model_means$beta_0[model_means$scale == "Commune"], beta_pop = 0, 
-               sigma_0 = model_means$sigma_0[model_means$scale == "Commune"], 
-               known_alphas = NA, 
-               covar_name = "ttimes", pop_predict = "flatPop", intercept = "random",
-               summed = FALSE, ctar_bump = FALSE, data_source = "National", 
-               scale = "Commune",
-               trans = 1e5, known_catch = FALSE, 
-               p_rab_min = 0.2, p_rab_max = 0.6, rho_max = 0.98,
-               max_HDR = 25, min_HDR = 5, 
-               dog_rabies_inc = 0.01, human_exp_rate = 0.39, 
-               prob_death = 0.16, nsims = 1000, scenario = commune_df$scenario) -> commune_deaths
-
-
-commune_deaths <- data.table(commune_deaths)
-commune_grouped <- commune_deaths[, .(deaths_mean = sum(deaths_mean)), by = scenario]
-ggplot(data = commune_grouped, aes(x = scenario, y = deaths_mean)) + geom_line() + expand_limits(y = 0)
-
+## This should for each model apply the respective params and get out all the preds for bites/deaths/vials
+foreach(params = iter(model_means, by = 'row'), .combine = multicomb) %do% {
+  foreach(dt = iter(comm_by_scenario), .combine = multicomb) %do% {
+    if(params$scale == "District") {
+      dt$weighted_times <- dt$weighted_times_dist
+    }
+    predict.all(ttimes = dt$weighted_times/60, pop = dt$pop, catch = dt$catch_numeric, 
+                names = dt$commune_id, beta_ttimes = params$beta_access, beta_0 = params$beta_0, 
+                beta_pop = 0, sigma_0 = params$sigma_0, known_alphas = NA, 
+                pop_predict = params$pop_predict, intercept = params$intercept, 
+                data_source = params$data_source, scale = params$scale, trans = 1e5, known_catch = FALSE, 
+                p_rab_min = 0.2, p_rab_max = 0.6, rho_max = 0.98, max_HDR = 25, min_HDR = 5, 
+                dog_rabies_inc = 0.01, human_exp_rate = 0.39, prob_death = 0.16, nsims = 1000,
+                outputs = c("bites", "deaths", "vials"), scenario = dt$scenario) -> predictions
+  } -> scale_preds 
+}-> all_preds
 
 ## Single catchment
 commune_max <- fread("output/ttimes/max_commune.csv")
@@ -133,7 +94,7 @@ predict.deaths(access = commune_max$weighted_times/60, ctar_in = 0,
                prob_death = 0.16, nsims = 1000, scenario = commune_max$scenario) -> comm_max
 sum(comm_max$deaths_mean, na.rm = TRUE)
 
-## Hypothetica
+## Hypotheticals
 comm_model <- model_means[model_means$scale == "District", ]
 ttimes <- seq(0, 15, by = 0.2)
 pop_plot <- 1e5
