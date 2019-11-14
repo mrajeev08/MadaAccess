f468c3184703cf0fa518ac29ef0ac1ab1501eb99
Malavika Rajeev <mrajeev08@gmail.com>
2019-11-13 22:29:49 -0500

    fixing way max and baselines are factored

--- R/05_incremental/03_postprocess_ttimes.R
+++ R/05_incremental/03_postprocess_ttimes.R
@@ -20,38 +20,29 @@ district_df <- district_df[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = T
                            by = .(district_id, scenario)]
 district_max <- fread("output/ttimes/max_district.csv")
 district_max <- district_max[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRUE)], 
-                           by = .(district_id, scenario)]
-district_max[, c("scenario", "base_catches", "max_catches", 
-                "clinic_added") := .(1648, max_catches, NULL, 1648)]
+                           by = district_id]
+district_max[, c("scenario", "clinic_added") := .(1648, NA)]
 district_baseline <- fread("output/ttimes/baseline_district.csv")
-district_baseline <- district_baseline[scenario == 31][, .SD[prop_pop_catch == max(prop_pop_catch, 
-                                                                                   na.rm = TRUE)], 
-                                                       by = .(district_id, scenario)]
-district_baseline$scenario <- 0
+district_baseline <- district_baseline[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRUE)], 
+                                                       by = district_id]
+district_baseline[, clinic_added := NA]
 
-district_max <- district_max
 ## Commune all and max
 commune_df <- fread("output/ttimes/incremental_commune.csv")
 commune_df <- commune_df[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRUE)], 
                          by = .(commune_id, scenario)]
 commune_max <- fread("output/ttimes/max_commune.csv")
 commune_max <- commune_max[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRUE)], 
-                           by = .(commune_id, scenario)]
-commune_max[, c("scenario", "base_catches", "max_catches", 
-                "clinic_added") := .(1648, max_catches, NULL, 1648)]
+                           by = commune_id]
+commune_max[, c("scenario", "clinic_added") := .(1648, NA)]
 commune_baseline <- fread("output/ttimes/baseline_commune.csv")
-commune_baseline <- commune_baseline[scenario == 31][, .SD[prop_pop_catch == max(prop_pop_catch, 
-                                                                                   na.rm = TRUE)], 
-                                                       by = .(commune_id, scenario)]
-commune_baseline$scenario <- 0
+commune_baseline <- commune_baseline[, .SD[prop_pop_catch == max(prop_pop_catch, na.rm = TRUE)], 
+                                                       by = commune_id]
+commune_baseline[, clinic_added := NA]
 
 ## Rbind them 
 commune_master <- rbind(commune_baseline, commune_df, commune_max)
 district_master <- rbind(district_baseline, district_df, district_max)
-
-## Fix so that catches are numeric within the scenarios
-district_master[, catch_numeric := as.numeric(as.factor(base_catches)), by = scenario]
-commune_master[, catch_numeric := as.numeric(as.factor(base_catches)), by = scenario]
 
 ## Match district ttimes to commune ids to get 
 mada_communes$match_id <- 1:nrow(mada_communes@data)
