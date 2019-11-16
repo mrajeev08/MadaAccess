78208784f2cec0169a317018303348cc0bf6907d
Malavika Rajeev <mrajeev08@gmail.com>
2019-11-16 18:41:04 -0500

    :bug: issue with districts with no names to match
    was resulting in duplicates

--- R/02_bitedata/01_match_names.R
+++ R/02_bitedata/01_match_names.R
@@ -1,6 +1,6 @@
 ####################################################################################################
-##' CLeaning district and commune names 
-##' Details: Using some manual and some automated name matching for administrative units for the three
+##' Cleaning district and commune names 
+##' Details: Using some manual and some fuzzy  matching for administrative units for the three
 ##' data sets 
 ##' Author: Malavika Rajeev 
 ####################################################################################################
@@ -11,48 +11,52 @@ library(stringdist)
 library(rgdal)
 library(dplyr)
 
-## source matching function
+##' source matching function
+source("R/functions/utils.R")
 source("R/functions/match.names.R")
 
-## Read in shapefiles
+##' Read in shapefiles
 mada_communes <- readOGR("data/processed/shapefiles/mada_communes.shp")
 mada_districts <- readOGR("data/processed/shapefiles/mada_districts.shp")
 
-## match commune names in peripheral data (leave manually unmatched but any with below 3 match to name)
+##' match commune names in peripheral data (leave manually unmatched but any with below 3 match to name)
 peripheral <- read.csv("data/raw/bitedata/peripheral/SaisieRage_DATA_2018-09-21_1755.csv")
 peripheral$distcode <- paste0(substring(peripheral$district, 1, 1), 
                               substring(peripheral$district, 3, 8))
-peripheral_comm_matches <- match.admin(data_names = peripheral$commune, data_nest = peripheral$distcode, 
-             match_names = mada_communes$ADM3_EN, match_nest = mada_communes$distcode,
-             match_method = "osa", nested = TRUE)
-write.csv(peripheral_comm_matches, "data/raw/match_names/peripheral_comm_matches.csv", row.names = FALSE)
+peripheral_comm_matches <- match.admin(data_names = peripheral$commune, 
+                                       data_nest = peripheral$distcode, 
+                                       match_names = mada_communes$commune, 
+                                       match_nest = mada_communes$distcode,
+                                       match_method = "osa", nested = TRUE)
+write.csv(peripheral_comm_matches, "data/raw/match_names/peripheral_comm_matches.csv", 
+          row.names = FALSE)
 
-## Also get table of notes to match to see if known Category 1 (manually match afterwards)
+##' Also get table of notes to match to see if known Category 1 (manually match afterwards)
 peripheral %>%
   group_by(notes = remarque) %>%
   summarize(Freq = n()) -> notes_to_match
 write.csv(notes_to_match, "data/raw/match_names/peripheral_notes_to_match.csv", row.names = FALSE)
 
-## match district names in IPM
+##' match district names in IPM
 load("data/raw/bitedata/ipm/ipm.rda")
 head(IPM)
 ipm_dist_matches <- match.admin(data_names = IPM$fiv, data_nest = NULL, 
-                                match_names = mada_districts$ADM2_EN, match_nest = NULL,
+                                match_names = mada_districts$district, match_nest = NULL,
                                 match_method = "osa", nested = FALSE)
 write.csv(ipm_dist_matches, "data/raw/match_names/ipm_dist_matches.csv", row.names = FALSE)
 
-## after manual matching of names in IPM data
+##' after manual matching of names in IPM data
 names_matched <- read.csv("data/processed/matched_names/ipm_dist_matched.csv")
 names_matched$distcode <- mada_districts$distcode[match(names_matched$match, 
-                                                        tolower(mada_districts$ADM2_EN))]
+                                                        tolower(mada_districts$district))]
 IPM$distcode <- names_matched$distcode[match(tolower(IPM$fiv), names_matched$names_tomatch)]
 ipm_comm_matches <- match.admin(data_names = IPM$fir, data_nest = IPM$distcode, 
-                                match_names = mada_communes$ADM3_EN, 
+                                match_names = mada_communes$commune, 
                                 match_nest = mada_communes$distcode, 
                                 match_method ="osa", nested = TRUE)
 write.csv(ipm_comm_matches, "data/raw/match_names/ipm_comm_matches.csv", row.names = FALSE)
 
-## match district names in Moramanga data
+##' match district names in Moramanga data
 moramanga <- read.csv("data/raw/bitedata/moramanga/CTAR_%28V3%29_20190918150219.csv")
 moramanga$commune <- sapply(strsplit(as.character(moramanga$Patient.Home), "\\("), "[", 1)
 moramanga$commune <- trimws(moramanga$commune, which = "right")
@@ -60,23 +64,26 @@ moramanga$district <- sapply(strsplit(as.character(moramanga$Patient.Home), "\\,
 moramanga$district <- gsub(" \\(District\\)", "", moramanga$district)
 
 moramanga_dist_matches <- match.admin(data_names = moramanga$district, data_nest = NULL, 
-                                      match_names = mada_districts$ADM2_EN, match_nest = NULL, 
+                                      match_names = mada_districts$district, match_nest = NULL, 
                                       match_method = "osa", nested = FALSE)
 write.csv(moramanga_dist_matches, "data/raw/match_names/moramanga_dist_matches.csv", row.names = FALSE)
 
-## Pull in manually matched districts
-## Then automatically match communes
+##' Pull in manually matched districts
+##' Then automatically match communes
 moramanga_dist_matches <- read.csv("data/processed/matched_names/moramanga_dist_matched.csv")
 moramanga_dist_matches$distcode <- mada_districts$distcode[match(moramanga_dist_matches$match, 
-                                                        tolower(mada_districts$ADM2_EN))]
+                                                        tolower(mada_districts$district))]
 moramanga$distcode <- moramanga_dist_matches$distcode[match(tolower(moramanga$district), 
                                                    moramanga_dist_matches$names_tomatch)]
 moramanga_comm_matches <- match.admin(data_names = moramanga$commune, 
                                       data_nest = moramanga$distcode,
-                                      match_names = mada_communes$ADM3_EN, 
+                                      match_names = mada_communes$commune, 
                                       match_nest = mada_communes$distcode, 
                                       match_method = "osa", nested = TRUE)
 moramanga_comm_matches <- filter(moramanga_comm_matches, !is.na(names_tomatch))
 write.csv(moramanga_comm_matches, "data/raw/match_names/moramanga_comm_matches.csv", 
           row.names = FALSE)
+
+##' Saving session info
+out.session(path = "R/02_bitedata/01_match_names.R", filename = "sessionInfo.csv")
 
