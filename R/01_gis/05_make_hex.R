ab4212bf60b74cf1869639c8c12d19069781a244
Malavika Rajeev <mrajeev08@gmail.com>
2019-11-22 20:07:33 -0500

    wip with manual district assignment

--- R/01_gis/05_make_hex.R
+++ R/01_gis/05_make_hex.R
@@ -14,80 +14,25 @@ library(rgdal) # for shapefiles (also comes with sp)
 library(raster) # for rasters and resampling
 library(geogrid)
 library(data.table)
+library(geosphere)
+library(rgeos)
+library(spdep)
+library(tidyverse)
 source("R/functions/utils.R")
 
 ##' Load in GIS files 
 mada_communes <- readOGR("data/processed/shapefiles/mada_communes.shp")
 mada_districts <- readOGR("data/processed/shapefiles/mada_districts.shp")
-friction_masked <- raster("data/processed/rasters/friction_mada_masked.tif")
-ctar_metadata <- fread("data/raw/ctar_metadata.csv")
 
-## Getting ctar district and commune
-ctar_metadata$ctar_dist <- mada_districts$distcode[match(ctar_metadata$District, 
-                                                         mada_districts$district)]
-pts <- SpatialPoints(cbind(ctar_metadata$LONGITUDE, ctar_metadata$LATITUDE), 
-                     proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-ctar_metadata$ctar_comm <- over(pts, mada_communes)$commcode
-mada_districts$ctar_in_dist <- ifelse(mada_districts$distcode %in% ctar_metadata$ctar_dist, 1, 0)
-mada_communes$ctar_in_comm <- ifelse(mada_communes$commcode %in% ctar_metadata$ctar_comm, 1, 0)
+##' Communes manually assigned
+communes_hex <- readOGR("data/processed/shapefiles/mada_communes_hex.shp")
+communes_hex@data %>% group_by(distcode) %>% summarize(ncomms_man = n()) -> check
+mada_communes@data %>% group_by(distcode) %>% summarize(ncomms = n()) -> ncomms
+ncomms %>%
+  left_join(check) -> check
+check %>% mutate(diff = ncomms - ncomms_man) -> check
 
-## Hexagonal maps
-## Check the potentials, here seed 1 looks the best
-# for (i in 1:6) {
-#   new_cells <- calculate_grid(shape = mada_districts, grid_type = "hexagonal", seed = i)
-#   plot(new_cells, main = paste("Seed", i, sep = " "))
-# }
-ref_against <- function(x, base_ref) {
-  check <- ifelse(x > base_ref, NA, x)
-  min(check, na.rm = TRUE)
-} 
-
-library(geosphere)
-library(rgeos)
-new_cells <- calculate_grid(shape = mada_districts, grid_type = "hexagonal", seed = 1)
-# shape_pts <- coordinates(mada_districts)
-shape_pts <- gCentroid(mada_districts, byid = TRUE)
-cell_pts <- new_cells[[1]]
-
-dist_mat <- distm(shape_pts, cell_pts)
-colnames(dist_mat) <- 1:114
-rownames(dist_mat) <- mada_districts$distcode
-hex_id <- apply(dist_mat, 1, which.min)
-base_ref <- apply(dist_mat, 1, min)
-merge_df <- data.table(hex_id = 1:114)
-match_df <- data.table(distcode = rownames(dist_mat), hex_id, base_ref)
-match_df <- match_df[, .SD[base_ref == min(base_ref)], by = hex_id]
-match_df <- match_df[merge_df, on = "hex_id"] ## NAs for ones with no matches or multiple matches
-match_df$base_ref[is.na(match_df$base_ref)] <- Inf
-setorder(match_df, hex_id)
-dist_mat_now <- dist_mat[!(rownames(dist_mat) %in% match_df$distcode), ] # Only rows without a unique match
-print(nrow(dist_mat_now))
-
-while(!is.null(dim(dist_mat_now))) {
-  updated <- apply(dist_mat_now, 1, ref_against, base_ref = match_df$base_ref)
-  check <- which(dist_mat_now == updated, arr.ind = TRUE)
-  check_base <- which(dist_mat_now == updated)
-  updated <- data.table(distcode = rownames(check), hex_id = check[, 2], 
-                        base_ref = dist_mat_now[check_base])
-  match_df <- rbind(match_df, updated)
-  match_df <- match_df[, .SD[base_ref == min(base_ref)], by = hex_id]
-  dist_mat_now <- dist_mat[!(rownames(dist_mat) %in% match_df$distcode), ] # Only rows without a unique match
-  print(nrow(dist_mat_now))
-}
-
-match_df$distcode[is.na(match_df$distcode)] <- rownames(dist_mat)[!(rownames(dist_mat) 
-                                                                         %in% match_df$distcode)]
-shape_cells <- new_cells[[2]]
-shape_cells$hex_id <- 1:length(shape_cells)
-shape_cells$distcode <- match_df$distcode[match(shape_cells$hex_id, merge_df$hex_id)]
-
-## Compare to
-shape_cells@data <- dplyr::left_join(shape_cells@data, mada_districts@data)
-district_hex <- assign_polygons(mada_districts, new_cells)
-writeOGR(shape_cells, dsn = "data/processed/shapefiles", layer = "mada_districts_hex_check", 
-         driver = "ESRI Shapefile", overwrite_layer = TRUE)
-writeOGR(district_hex, dsn = "data/processed/shapefiles", layer = "mada_districts_hex", 
-         driver = "ESRI Shapefile", overwrite_layer = TRUE)
+## For each 
 
 ## Commune
 ## Check the potentials, here seed 3 looks the best
@@ -96,46 +41,109 @@ writeOGR(district_hex, dsn = "data/processed/shapefiles", layer = "mada_district
 #   plot(new_cells, main = paste("Seed", i, sep = " "))
 # }
 
-new_cells <- calculate_grid(shape = mada_communes, grid_type = "hexagonal", seed = 5)
-shape_pts <- coordinates(mada_communes)
-cell_pts <- new_cells[[1]]
+comm_cells <- calculate_grid(shape = mada_communes, grid_type = "hexagonal", seed = 5)
+cell_pts <- comm_cells[[1]]
 
-dist_mat <- distm(shape_pts, cell_pts)
-colnames(dist_mat) <- 1:length(mada_communes)
-rownames(dist_mat) <- mada_communes$commcode
-hex_id <- apply(dist_mat, 1, which.min)
-unique <- names(table(hex_id)[table(hex_id)  == 1])
-hex_left <- names(table(hex_id)[table(hex_id) > 1])
-check <- rep(NA, length(hex_left))
-for (i in 1:length(hex_left)) {
-  check[i] <- names(which.min(dist_mat[hex_id == hex_left[i], hex_left[i]]))
-}
-hex_id[hex_id %in% hex_left] <- NA
-hex_id[check] <- hex_left
-dist_mat_now <- dist_mat[is.na(hex_id), !(colnames(dist_mat) %in% hex_id)] # Only rows without a unique match
+## Assigning cells to communes
+## Inputs are district distance matrix
+district_pts <- gCentroid(mada_districts, byid = TRUE)
+district_pts$distcode <- mada_districts$distcode
 
-while(!is.null(dim(dist_mat_now))) {
-  ## Should always be symmetrical (ncols = nrows)
-  hex_id_next <- colnames(dist_mat_now)[apply(dist_mat_now, 1, which.min)]
-  names(hex_id_next) <- rownames(dist_mat_now)
+## Count of communes in each district
+mada_communes@data %>%
+  group_by(distcode) %>%
+  summarize(ncomms = n()) -> dist_comms
+dist_comms <- as.data.table(dist_comms)
+
+## Adjacency data frame for comm cells
+adj_mat <- poly2nb(comm_cells[[2]], row.names = 1:1579)
+adj_mat <- nb2mat(adj_mat, style = "B", zero.policy = TRUE)
+colnames(adj_mat) <- rownames(adj_mat)
+
+## If you want to start over start here
+adj_pairs <- melt(data.table(cell_id = rownames(adj_mat), adj_mat), id.vars = "cell_id")
+colnames(adj_pairs) <- c("from", "to", "adj")
+adj_pairs <- adj_pairs[adj != 0]
+
+## District distance matrix
+dist_mat <- distm(district_pts)
+rownames(dist_mat) <- colnames(dist_mat) <- mada_districts$distcode
+
+## Degree matrix for districts
+bearing_mat <- apply(district_pts@coords, 1, function(x) bearing(x, district_pts@coords))
+rownames(bearing_mat) <- colnames(bearing_mat) <- mada_districts$distcode
+
+## Cell to district distance matrix
+cell_to_dist <- distm(cell_pts, district_pts)
+colnames(cell_to_dist) <- mada_districts$distcode
+cell_to_dist <- data.table(to = as.factor(1:1579), cell_to_dist)
+adj_pairs <- adj_pairs[cell_to_dist, on = "to"]
+
+## Seed cell and district id
+seed_dist <- "MG51507"
+seed_cell <- 39
+
+out_dt <- data.table(distcode = NA, cell_id = NA)
+## Loop to assign cells
+while(nrow(adj_pairs) > 0) {
+  adj_now <- adj_pairs[from == seed_cell]
+  ncells <- dist_comms[distcode == seed_dist]$ncomms
+  ids <- ids_added <- unique(adj_now$to) 
   
-  unique <- names(table(hex_id_next)[table(hex_id_next)  == 1])
-  hex_left <- names(table(hex_id_next)[table(hex_id_next) > 1])
-  
-  check <- rep(NA, length(hex_left))
-  for (i in 1:length(hex_left)) {
-    check[i] <- names(which.min(dist_mat_now[hex_id_next == hex_left[i], hex_left[i]]))
+  while(ncells - length(ids) > 0) {
+    adj_now <- adj_pairs[from %in% ids_added]
+    ids_added <- unique(adj_now$to)
+    ids <- c(ids, ids_added)
   }
-  hex_id[names(hex_id) %in% check] <- hex_left
-  dist_mat_now <- dist_mat[is.na(hex_id), !(colnames(dist_mat) %in% hex_id)] # Only rows without a unique match
-  print(nrow(dist_mat_now))
+  
+  ids <- c(seed_cell, ids)
+  ids <- ids[1:ncells] ## so as not to get more than you need
+  adj_pairs <- adj_pairs[!(to %in% ids)] ## get rid of already assigned ones as options
+  
+  ## pick new seed distcode (closest district to current dist seed)
+  seed_dist_old <- seed_dist
+  seed_dist <- names(which.min(dist_mat[seed_dist, colnames(dist_mat) != seed_dist]))
+  ## Remove old district from dist mat
+  dist_mat <- dist_mat[rownames(dist_mat) != seed_dist_old, colnames(dist_mat) != seed_dist_old]
+  
+  ## pick new seed cell id
+  ## get bearings to candidate cell ids
+  cands <- adj_pairs[from %in% ids]
+  if(nrow(cands) == 0) {
+    cands <- adj_pairs
+    cands$distance <- adj_pairs[, colnames(adj_pairs) == seed_dist, with = FALSE] 
+    ## pick ones closest to new district? or to old district?
+    setorder(cands, distance)
+    cands <- cands[1:10, ]
+  }
+  
+  ref_bearing <- bearing_mat[seed_dist_old, seed_dist]
+  bearing_mat <- bearing_mat[rownames(bearing_mat) != seed_dist_old, 
+                             colnames(bearing_mat) != seed_dist_old]
+  
+  bearing_cands <- bearing(district_pts[district_pts$distcode == seed_dist_old, ], 
+                           cell_pts[cands$to])
+  
+  seed_cell <- cands$to[which.min(abs(bearing_cands - ref_bearing))] 
+  
+  print(seed_dist)
+  
+  ## Make data frame and rbind
+  now_dt <- data.table(distcode = seed_dist_old, cell_id = ids)
+  out_dt <- rbind(out_dt, now_dt)
 }
-hex_id[is.na(hex_id)] <- colnames(dist_mat)[!(colnames(dist_mat) %in% hex_id)]
-merge_df <- data.frame(commcode = names(hex_id), row_id = hex_id)
-shape_cells <- new_cells[[2]]
-shape_cells$row_id <- 1:length(shape_cells)
-shape_cells$commcode <- merge_df$commcode[match(shape_cells$row_id, merge_df$row_id)]
-shape_cells@data <- dplyr::left_join(mada_communes@data, shape_cells@data)
 
-writeOGR(shape_cells, dsn = "data/processed/shapefiles", layer = "mada_communes_hex", 
+comm_hex <- comm_cells[[2]]
+comm_hex$cell_id <- as.character(1:1579)
+gg_comm <- fortify(comm_hex, region = "cell_id")
+out_dt$cell_id <- as.character(out_dt$cell_id)
+gg_comm <- left_join(gg_comm, out_dt, by = c("id" = "cell_id"))
+
+ggplot(data = gg_comm, aes(x = long, y = lat, group = group, fill = distcode)) +
+  geom_polygon() +
+  scale_fill_discrete(guide = "none")
+
+comm_hex@data <- left_join(comm_hex@data, out_dt)
+
+writeOGR(comm_hex, dsn = "data/processed/shapefiles", layer = "test_it", 
          driver = "ESRI Shapefile", overwrite_layer = TRUE)
