c7e77c6fee8d3c0c1e2d87698c71650e72d60d84
Malavika Rajeev <mrajeev08@gmail.com>
2019-12-04 15:04:30 -0500

    fixing vizualizing max access

--- R/figures/figM5.R
+++ R/figures/figM5.R
@@ -24,7 +24,7 @@ vial_preds %>%
   summarize_at(vars(vials_mean:vials_lower), sum, na.rm = TRUE) -> natl_vials
 
 natl_preds_all <- left_join(natl_preds, natl_vials)
-natl_preds_all$scenario[natl_preds_all$scenario == max(natl_preds_all$scenario)] <- 550
+natl_preds_all$scenario[natl_preds_all$scenario == max(natl_preds_all$scenario)] <- 600
 
 ##' Colors
 scale_levs <- c("Commune", "District")
@@ -32,35 +32,37 @@ model_cols <- wesanderson::wes_palettes$Rushmore1[c(3, 4)]
 names(model_cols) <- scale_levs 
 
 ##' Figure 5.A
-figM5.A <- ggplot(data = natl_preds_all, aes(x = scenario, y = deaths_mean, color = scale, 
-                                             fill = scale, 
+figM5.A <- ggplot(data = natl_preds_all, aes(x = scenario, y = deaths_mean, color = scale, fill = scale, 
                                   shape = scale)) +
   geom_point(size = 2) +
   geom_linerange(aes(ymin = deaths_upper, ymax = deaths_lower), alpha = 0.35) +
   scale_color_manual(values = model_cols, name = "Model scale") +
   scale_fill_manual(values = model_cols, name = "Model scale") +
   scale_shape_manual(values = c(22, 23), name = "Model scale") +
-  scale_x_continuous(breaks = c(0, 100, 200, 300, 400, 472, 550), 
+  scale_x_continuous(breaks = c(0, 100, 200, 300, 400, 472, 600), 
                      labels = c(0, 100, 200, 300, 400, 472, "max (1648)")) +
-  annotate("rect", xmin = 485, xmax = 540, ymin = 0, ymax = Inf, fill = "white") +
-  labs(x = "# additional clinics \n providing PEP", y = "Mean predicted deaths \n nationally") +
-  coord_cartesian(ylim = c(50, max(natl_preds_all$deaths_upper)))
-
+  annotate(geom = "text", x = 520, y = -30, label = "......") +
+  labs(x = "# additional clinics \n providing PEP", y = "Mean deaths \n nationally",
+       tag = "A") +
+  coord_cartesian(clip = "off", ylim = c(0, max(natl_preds_all$deaths_upper)))
+  
 ##' Figure 5.B
 figM5.B <- ggplot(data = natl_preds_all, aes(x = scenario, y = vials_mean/deaths_mean, 
                                   color = scale, fill = scale, 
                                   shape = scale)) +
   geom_point(size = 2) +
-  geom_linerange(aes(ymin = vials_upper/deaths_upper, ymax = vials_lower/deaths_lower), 
+  geom_linerange(aes(ymax = vials_upper/deaths_upper, ymin = vials_lower/deaths_lower), 
                  alpha = 0.35) +
   scale_color_manual(values = model_cols, name = "Model scale") +
   scale_fill_manual(values = model_cols, name = "Model scale") +
   scale_shape_manual(values = c(22, 23), name = "Model scale") +
-  scale_x_continuous(breaks = c(0, 100, 200, 300, 400, 472, 550), 
+  scale_x_continuous(breaks = c(0, 100, 200, 300, 400, 472, 600), 
                      labels = c(0, 100, 200, 300, 400, 472, "max (1648)")) +
-  annotate("rect", xmin = 480, xmax = 540, ymin = 0, ymax = Inf, fill = "white") +
-  labs(x = "# additional clinics \n providing PEP", y = "Mean predicted deaths \n nationally") +
-  coord_cartesian(ylim = c(100, 800))
+  annotate(geom = "text", x = 520, y = 1, label = "......") +
+  labs(x = "# additional clinics \n providing PEP", y = "Mean vials per death \n nationally",
+       tag = "B") +
+  ylim(c(0, max(natl_preds_all$vials_lower/natl_preds_all$deaths_lower))) + 
+  coord_cartesian(clip = "off", ylim = c(50, 800))
 
-figM5 <- figM5.A /figM5.B
-ggsave("figs/M5.jpeg", figM5, device = "jpeg", height = 11, width = 8)
+figM5 <- figM5.A | figM5.B
+ggsave("figs/M5.jpeg", figM5, device = "jpeg", height = 8, width = 11)
