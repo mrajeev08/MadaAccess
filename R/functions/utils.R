0afde17f8811e7467b9213d1d108e6cd95db0f34
Malavika Rajeev <mrajeev08@gmail.com>
2019-12-31 16:40:26 -0500

    major rename!

--- R/functions/utils.R
+++ R/functions/utils.R
@@ -22,7 +22,7 @@ out.session <- function(path, filename = "sessionInfo.csv") {
   }
 }
 
-## ggplot helpers fir reordering within facets
+## ggplot helpers for reordering within facets
 reorder_within <- function(x, by, within, fun = mean, sep = "___", ...) {
   new_x <- paste(x, within, sep = sep)
   stats::reorder(new_x, by, FUN = fun)
@@ -36,33 +36,4 @@ scale_x_reordered <- function(..., sep = "___") {
 scale_y_reordered <- function(..., sep = "___") {
   reg <- paste0(sep, ".+$")
   ggplot2::scale_y_discrete(labels = function(x) gsub(reg, "", x), ...)
-}
-
-## Find nth most recent file
-find.bydate <- function(path, patt, ind, rank){
-  ## function to get latest file
-  ## path = the folder to look in, pattern = the pattern to match to
-  ## ind = where the date is stored in the name if you were to strsplit by '_'
-  ## rank = which file do you want? most recent = 1, second most recent = 2, etc.
-  ## will print warning if rank is greater than the # of files, but will give you the oldest one
-  
-  files <- list.files(path, pattern = patt)
-  
-  if (rank > length(files)){
-    print("Warning: rank greater than # of files, returning oldest file")
-    rank <- length(files)
-  }
-  
-  time_stamp <- rep (NA, length(files))
-  
-  for (i in 1:length(files)){
-    time_stamp[i] <- unlist(strsplit(files[i], "_"))[ind]
-  }
-  
-  time_stamp <- substr(as.character(time_stamp), 1, 8)
-  diff <- as.numeric(Sys.Date() - as.Date(time_stamp, "%Y%m%d"))
-  files <- files[order(diff)]
-  retrieved <- read.csv(paste0(path, files[rank]), fileEncoding = 'latin1')
-  print(paste0(path, files[rank]))
-  return(retrieved)
 }
