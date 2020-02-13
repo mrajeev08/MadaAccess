64ebbf411e68da33f5cb355e767aa03c7059b404
Malavika Rajeev <mrajeev08@gmail.com>
2020-02-13 13:34:26 -0500

    updated burden functions
    - also added HDR/p_exp/inc relationship functions

--- R/05_predictions/01_burden_incremental.R
+++ R/05_predictions/01_burden_incremental.R
@@ -1,34 +1,31 @@
-####################################################################################################
-##' Getting incremental estimates of burden 
-##' Details: Pulling in district and commune estimates of travel times as clinics are added 
-##' Author: Malavika Rajeev 
-####################################################################################################
+# ------------------------------------------------------------------------------------------------ #
+#' Getting incremental estimates of burden  
+#' Details: Pulling in district and commune estimates of travel times as clinics are added 
+# ------------------------------------------------------------------------------------------------ #
 
-##' Set up
-##' ------------------------------------------------------------------------------------------------
-rm(list = ls())
-
-##' Libraries and scripts
+# Set up ------------------------------------------------------------------------------------
 library(data.table)
 library(magrittr)
 library(foreach)
 library(doRNG)
 library(iterators)
 library(tidyverse)
+library(triangle)
+source("R/functions/out.session.R")
 source("R/functions/predict_functions.R")
 select <- dplyr::select
 
-## Read in data
-commune_master <- fread("output/ttimes/master_commune.csv")
-setorder(commune_master, commune_id, scenario)
-commune_master[, diff_comm := weighted_times - shift(weighted_times, 1), by = commune_id]
-commune_master[, diff_dist := weighted_times_dist - shift(weighted_times_dist, 1), by = commune_id]
+# Filter to only do ones for which travel times have changed
+commune_master <- fread("output/ttimes/commune_maxcatch.gz")
+setorder(commune_master, commcode, scenario)
+commune_master[, diff_comm := ttimes_wtd - shift(ttimes_wtd, 1), by = commcode]
+commune_master[, diff_dist := ttimes_wtd_dist - shift(ttimes_wtd_dist, 1), by = commcode]
 comm_run <- commune_master[diff_comm != 0 | scenario %in% c(0, 1648)]
 dist_run <- commune_master[diff_dist != 0 | scenario %in% c(0, 1648)]
-rm(commune_master) ## cleaning up memory!
+rm(commune_master) # cleaning up memory!
 gc()
 
-##' Get model means for commune and district models
+# Get model means for commune and district models
 model_ests <- read.csv("output/mods/estimates.csv")
 model_ests %>%
   dplyr::select(params, Mean, pop_predict, intercept, data_source, scale) %>%
@@ -36,27 +33,26 @@ model_ests %>%
   dplyr::filter(pop_predict == "flatPop", data_source == "National", 
                 intercept == "random") -> model_means
 
-##' Commune predictions 
-##' ------------------------------------------------------------------------------------------------
+# Commune predictions -----------------------------------------------------------------------
 params <- model_means[model_means$scale == "Commune", ]
-bite_mat <- predict.bites(ttimes = comm_run$weighted_times/60, pop = comm_run$pop, 
-                          catch = comm_run$base_catches, names = comm_run$commune_id, 
+
+bite_mat <- predict.bites(ttimes = comm_run$ttimes_wtd/60, pop = comm_run$pop, 
+                          catch = comm_run$catchment, names = comm_run$commcode, 
                           beta_ttimes = params$beta_ttimes, beta_0 = params$beta_0, 
                           beta_pop = 0, sigma_0 = params$sigma_0, known_alphas = NA, 
                           pop_predict = params$pop_predict, intercept = params$intercept, 
-                          trans = 1e5, known_catch = FALSE, nsims = 1000)
+                          trans = 1e5, known_catch = FALSE, nsims = 1000, type = "inc")
 all_mats <-  predict.deaths(bite_mat, pop = comm_run$pop,
-                           p_rab_min = 0.2, p_rab_max = 0.6, rho_max = 0.98,
-                           max_HDR = 35, min_HDR = 7, dog_rabies_inc = 0.01, 
-                           human_exp_rate = 0.39, 
-                           prob_death = 0.16)
+                           p_rab_min = 0.2, p_rab_max = 0.6,
+                           rho_max = 0.98, exp_min = 15/1e5, exp_max = 76/1e5,
+                           prob_death = 0.16, dist = "uniform")
 
 all_mats <- c(list(bites = bite_mat), all_mats)
 
 foreach(i = 1:length(all_mats), .combine = 'cbind', .packages = "data.table") %do% {
     mat <- all_mats[[i]]
     labels <- paste0(names(all_mats)[i], "_", c("mean", "upper", "lower"))
-    mean <- rowMeans(mat, na.rm = TRUE) ## mean for each row = admin unit
+    mean <- rowMeans(mat, na.rm = TRUE) # mean for each row = admin unit
     sd <- apply(mat, 1, sd, na.rm = TRUE)
     upper <- mean + 1.96*sd/sqrt(ncol(mat))
     lower <- mean - 1.96*sd/sqrt(ncol(mat))
@@ -65,49 +61,30 @@ foreach(i = 1:length(all_mats), .combine = 'cbind', .packages = "data.table") %d
     out
   } -> admin_comm
 
-admin_comm <- data.table(names = comm_run$commune_id,
-                         ttimes = comm_run$weighted_times/60, pop = comm_run$pop, 
-                         catch = comm_run$base_catches, scenario = comm_run$scenario, 
+admin_comm <- data.table(names = comm_run$commcode,
+                         ttimes = comm_run$ttimes_wtd/60, pop = comm_run$pop, 
+                         catch = comm_run$catchment, scenario = comm_run$scenario, 
                          scale = "Commune", admin_comm)
-##' For simulating vials and doing scenario analysis
-catch_comm <- data.table(bite_mat, catch = comm_run$base_catches, scenario = comm_run$scenario, 
-                         names = comm_run$commune_id)
-catch_comm %>%
-  complete(scenario = 0:472, names) %>%
-  group_by(names) %>%
-  arrange(scenario) %>%
-  fill(3:ncol(catch_comm), .direction = "down") -> catch_comm
-catch_comm <- as.data.table(catch_comm)[, names := NULL]
-catch_comm <- catch_comm[, lapply(.SD, sum, na.rm = TRUE), by = c("catch", "scenario")]
-catch_mat <- as.matrix(catch_comm[, -c("catch", "scenario"), with = FALSE])
 
-## Filter to only the catchments that have changed
-catch_comm[, check := rowSums(catch_mat)]
-setorder(catch_comm, catch, scenario)
-catch_comm[, diff := (check - shift(check, 1)), by = catch]
-catch_comm <- catch_comm[diff != 0 | scenario %in% c(0, 1648)]
-
-##' District preds 
-##' ------------------------------------------------------------------------------------------------
+# District predictions -----------------------------------------------------------------------
 params <- model_means[model_means$scale == "District", ]
-bite_mat <- predict.bites(ttimes = dist_run$weighted_times/60, pop = dist_run$pop, 
-                          catch = dist_run$base_catches, names = dist_run$commune_id, 
+bite_mat <- predict.bites(ttimes = dist_run$ttimes_wtd/60, pop = dist_run$pop, 
+                          catch = dist_run$catchment, names = dist_run$commcode, 
                           beta_ttimes = params$beta_ttimes, beta_0 = params$beta_0, 
                           beta_pop = 0, sigma_0 = params$sigma_0, known_alphas = NA, 
                           pop_predict = params$pop_predict, intercept = params$intercept, 
-                          trans = 1e5, known_catch = FALSE, nsims = 1000)
+                          trans = 1e5, known_catch = FALSE, nsims = 1000, type = "inc")
 all_mats <-  predict.deaths(bite_mat, pop = dist_run$pop,
-                            p_rab_min = 0.2, p_rab_max = 0.6, rho_max = 0.98,
-                            max_HDR = 35, min_HDR = 7, dog_rabies_inc = 0.01, 
-                            human_exp_rate = 0.39, 
-                            prob_death = 0.16)
+                            p_rab_min = 0.2, p_rab_max = 0.6,
+                            rho_max = 0.98, exp_min = 15/1e5, exp_max = 76/1e5, 
+                            prob_death = 0.16, dist = "uniform")
 
 all_mats <- c(list(bites = bite_mat), all_mats)
 
 foreach(i = 1:length(all_mats), .combine = 'cbind', .packages = "data.table") %do% {
   mat <- all_mats[[i]]
   labels <- paste0(names(all_mats)[i], "_", c("mean", "upper", "lower"))
-  mean <- rowMeans(mat, na.rm = TRUE) ## mean for each row = admin unit
+  mean <- rowMeans(mat, na.rm = TRUE) # mean for each row = admin unit
   sd <- apply(mat, 1, sd, na.rm = TRUE)
   upper <- mean + 1.96*sd/sqrt(ncol(mat))
   lower <- mean - 1.96*sd/sqrt(ncol(mat))
@@ -116,32 +93,31 @@ foreach(i = 1:length(all_mats), .combine = 'cbind', .packages = "data.table") %d
   out
 } -> admin_dist
 
-admin_dist <- data.table(names = dist_run$commune_id,
-                         ttimes = dist_run$weighted_times/60, pop = dist_run$pop, 
-                         catch = dist_run$base_catches, scenario = dist_run$scenario, 
+admin_dist <- data.table(names = dist_run$commcode,
+                         ttimes = dist_run$ttimes_wtd/60, pop = dist_run$pop, 
+                         catch = dist_run$catchment, scenario = dist_run$scenario, 
                          scale = "District", admin_dist)
 
-##' For simulating vials and doing scenario analysis
-catch_dist <- data.table(bite_mat, catch = dist_run$base_catches, scenario = dist_run$scenario, 
-                         names = dist_run$commune_id)
-catch_dist %>%
-  complete(scenario = 0:472, names) %>%
-  group_by(names) %>%
+# Complete results
+# ------------------------------------------------------------------------------------------------
+burden_partial <- rbind(admin_dist, admin_comm)
+max_clinics <- max(comm_run$scenario[comm_run$scenario != 1648])
+
+burden_partial %>%
+  complete(scenario = 1:max_clinics, names, scale) %>% # from 1 to last
+  group_by(names, scale) %>%
   arrange(scenario) %>%
-  fill(3:ncol(catch_dist), .direction = "down") -> catch_dist
-catch_dist <- as.data.table(catch_dist)[, names := NULL]
-catch_dist <- catch_dist[, lapply(.SD, sum, na.rm = TRUE), by = c("catch", "scenario")]
-catch_mat <- as.matrix(catch_dist[, -c("catch", "scenario"), with = FALSE])
+  fill(4:ncol(burden_partial), .direction = "down") -> burden_complete
 
-## Filter to only the catchments that have changed
-catch_dist[, check := rowSums(catch_mat)]
-setorder(catch_dist, catch, scenario)
-catch_dist[, diff := (check - shift(check, 1)), by = catch]
-catch_dist <- catch_dist[diff != 0 | scenario %in% c(0, 1648)]
+## This should be true!
+burden_complete %>%
+  group_by(scenario, scale) %>%
+  summarize(n_admin = n()) %>%
+  group_by(scale) %>%
+  summarize(all(n_admin == 1579)) # should be true for all of them!
 
-##' Output results 
-##' ------------------------------------------------------------------------------------------------
-results_incremental <- rbind(admin_dist, admin_comm)
-fwrite(results_incremental, "output/preds/partial/burden_partial.csv")
-fwrite(catch_dist, "output/preds/partial/bites_bycatch_dist.csv")
-fwrite(catch_comm, "output/preds/partial/bites_bycatch_comm.csv")
+fwrite(burden_complete, "output/preds/burden/burden_complete.gz")
+
+##' Saving session info
+out.session(path = "R/05_predictions/01_burden_incremental.R", filename = "output/log_local.csv")
+
--- R/functions/predict_functions.R
+++ R/functions/predict_functions.R
@@ -1,27 +1,33 @@
-####################################################################################################
-##' Prediction functions for getting bites, deaths, and vials 
-##' Details: Wrapper function for getting bites, deaths, and vials from inputs 
-##' Author: Malavika Rajeev
-####################################################################################################
+# ------------------------------------------------------------------------------------------------ #
+#' Prediction functions for getting bites, deaths, and vials
+#' Details: Wrapper function for getting bites, deaths, and vials from inputs
+# ------------------------------------------------------------------------------------------------ #
 
-##' Function for getting bites from model inputs (return a matrix)
-##' ------------------------------------------------------------------------------------------------
+# 1. Function for getting bites from model inputs (return a matrix) -------------------------------
+#' Function for getting bite incidence or # of bites from model inputs
+#' Description
+#' Details
+#' @param Paramters
+#' @return Returned
+#' @section Dependencies:
+#'     List dependencies here, i.e. packages and other functions triangle
 predict.bites <- function(ttimes, pop, catch, names,
-                           beta_ttimes, beta_0, beta_pop, sigma_0, known_alphas, 
-                           pop_predict = "addPop", intercept = "random",
-                           trans = 1e5, known_catch = TRUE, nsims = 1000) {
+                          beta_ttimes, beta_0, beta_pop, sigma_0, known_alphas, 
+                          pop_predict = "addPop", intercept = "random",
+                          trans = 1e5, known_catch = TRUE, nsims = 1000, 
+                          type = "bites") {
   
   foreach(i = 1:nsims, .combine = cbind) %do% {
     if(intercept == "random") {
-      ## draw catchment level effects (pull this out for foreach)
-      ## first make catchment a factor and then drop levels and convert to numeric
+      # draw catchment level effects (pull this out for foreach)
+      # first make catchment a factor and then drop levels and convert to numeric
       catch_val <- as.numeric(droplevels(as.factor(catch)))
       
       alpha <- rnorm(max(catch_val, na.rm = TRUE), mean = beta_0, sd = sigma_0)
       alpha <- alpha[catch_val]
       
-      ## get alpha so that if known_catch is TRUE it pulls from estimates
-      ## known alphas have to be the same length as the data (potentially change this part!)
+      # get alpha so that if known_catch is TRUE it pulls from estimates
+      # known alphas have to be the same length as the data (potentially change this part!)
       if(known_catch == TRUE) {
         alpha <- ifelse(is.na(known_alphas) | known_alphas == 0, alpha, known_alphas)
       } 
@@ -51,59 +57,94 @@ predict.bites <- function(ttimes, pop, catch, names,
       if(pop_predict == "onlyPop") {
         exp_bites <- exp(beta_0 + beta_pop*pop/trans)
       }
-      exp_bites
     }
-    
-    bites <- rpois(length(exp_bites), exp_bites)
-    
+    if(type == "bites") {
+      exp_bites <- rpois(length(exp_bites), exp_bites)
+    }
+    exp_bites
   } -> bite_mat
   
   return(bite_mat)
 }
 
-## Function for getting deaths from input bites (return a matrix)
-## Work in incidence! (so you don't have to worry about binomial limitations)
+#  Predict deaths and other key decision tree outputs -----------------------------------------
+# Function for getting deaths from input bites (return a matrix)
+#' Title
+#' Description
+#' Details
+#' @param Paramters
+#' @return Returned
+#' @section Dependencies:
+#'     List dependencies here, i.e. packages and other functions
 predict.deaths <- function(bite_mat, pop,
-                           p_rab_min = 0.2, p_rab_max = 0.6, rho_max = 0.9,
-                           max_HDR = 25, min_HDR = 5, dog_rabies_inc = 0.01, 
-                           human_exp_rate = 0.39, 
-                           prob_death = 0.16) {
+                           p_rab_min = 0.2, p_rab_max = 0.6, p_rab_mode = NULL,
+                           rho_max = 0.9,
+                           exp_min = 15/1e5, exp_max = 76/1e5, exp_mode = NULL,
+                           prob_death = 0.16, dist = "uniform") {
   
-  ## Drawing rabies exposures
-  hdr <- runif(length(bite_mat), min = min_HDR, max = max_HDR)
+  # Getting rabies exposures incidence and p_rabid
+  if(dist == "uniform") {
+    human_exp_inc <- runif(n = length(bite_mat), min = exp_min, max = exp_max)
+    p_rabid <- runif(n = length(bite_mat), min = p_rab_min, max = p_rab_max)
+  }
+  
+  if(dist == "triangle") {
+    
+    if(is.null(p_rab_mode)) {
+      p_rab_mode <- (p_rab_min + p_rab_max)/2
+    }
+    
+    if(is.null(exp_mode)) {
+      exp_mode <- (exp_min + exp_max)/2
+    }
+    
+    human_exp_inc <- rtriangle(n = length(bite_mat), a = exp_min, b = exp_max)
+    p_rabid <- rtriangle(n = length(bite_mat), a = p_rab_min, b = p_rab_max)
+  }
+  
+  # Rabied exposures
   pop_vec <- rep(pop, ncol(bite_mat))
-  rabid_exps <- rpois(length(bite_mat), human_exp_rate*dog_rabies_inc*pop_vec/hdr)
+  rabid_exps <- human_exp_inc*pop_vec # expected rabid exposures
   
-  ## Getting proportion rabid
-  p_rabid <- runif(length(bite_mat), min = p_rab_min, max = p_rab_max)
-  reported_rabid <- bite_mat*p_rabid
-  max_reported <- rabid_exps*rho_max ## so that reporting is set at max
+  #  Rabid reported bites
+  reported_rabid <- bite_mat*p_rabid 
+  
+  # Constrain so that reporting is set at max
+  max_reported <- rabid_exps*rho_max 
   reported_rabid <- ifelse(reported_rabid > max_reported, max_reported, reported_rabid)
-  
-  ## Simulating deaths
-  deaths <- matrix(rbinom(length(bite_mat), round(rabid_exps - reported_rabid), prob_death),
-                   nrow = nrow(bite_mat), ncol = ncol(bite_mat))
-  averted <- matrix(rbinom(length(bite_mat), round(reported_rabid), prob_death), 
-                    nrow = nrow(bite_mat), ncol = ncol(bite_mat))
+
+  # Estimating reporting and proportion rabid 
+  # not binomially because we don't want to deal with zeros/undefined!
   p_rabid <- matrix(reported_rabid/as.vector(bite_mat), nrow = nrow(bite_mat), 
                     ncol = ncol(bite_mat))
   reporting <- matrix(reported_rabid/rabid_exps, nrow = nrow(bite_mat), 
                       ncol = ncol(bite_mat))
-  check <- list(deaths = deaths, averted = averted, p_rabid = p_rabid,
-                reporting = reporting)
   
-  return(check)
+  # Estimating deaths and deaths averted
+  unreported <- rpois(length(rabid_exps), rabid_exps - reported_rabid)
+  deaths <- matrix(rbinom(length(rabid_exps), size = unreported, prob = 0.16),
+                    nrow = nrow(bite_mat), ncol = ncol(bite_mat))
+  reported <- rpois(length(rabid_exps), reported_rabid)
+  averted <- matrix(rbinom(length(rabid_exps), size = reported, prob = prob_death), 
+                    nrow = nrow(bite_mat), ncol = ncol(bite_mat))
+  
+  out <- list(deaths = deaths, averted = averted, p_rabid = p_rabid,
+                reporting = reporting, rabid_exps = matrix(rabid_exps, nrow = nrow(bite_mat),
+                                                           ncol = ncol(bite_mat)))
+  
+  return(out)
 }
 
-##' Get burden or reporting (fixed) -------------------------------------------------------------
+# Get burden or reporting (fixed) -------------------------------------------------------------
+# Use human_exp_rate instead of incidence*hdr*rho_max!
 get.burden.fixed <- function(bite_mat, pop, hdr = 25, incidence = 0.01, 
-                             exp_rate = 0.39, p_rabid = 0.6, rho_max = 0.98, 
+                             exp_rate = 0.39, p_rabid = 0.6, rho_max = 0.9, 
                              prob_death = 0.16, scale = FALSE, slope = 1, intercept = 0, 
                              inc_max = inc100k_max, 
                              inc_min = inc100k_min) {
   
   if(scale == TRUE) {
-    ## Constrained and scaled incidence
+    # Constrained and scaled incidence
     inc <- slope*pop + intercept
     inc[inc > inc_max] <- inc_max
     inc[inc < inc_min] <- inc_min
@@ -115,17 +156,27 @@ get.burden.fixed <- function(bite_mat, pop, hdr = 25, incidence = 0.01,
   rabid_exps <- matrix(rabid_exps, nrow = nrow(bite_mat), 
                       ncol = ncol(bite_mat))
   
-  max_reported <- rabid_exps*rho_max ## so that reporting is set at max
+  max_reported <- rabid_exps*rho_max # so that reporting is set at max
   reported_rabid <- bite_mat*p_rabid
   reported_rabid <- ifelse(reported_rabid > max_reported, max_reported, reported_rabid)
   
-  ## Simulating deaths
+  # Simulating deaths
   deaths <- (rabid_exps - reported_rabid)*prob_death
   return(deaths)
 }
 
-##' Function for getting vials from input bites
-##' ------------------------------------------------------------------------------------------------
+
+# Helper functions for getting incidence from HDR/p_exp/dog_inc -------------------------------
+hdr_from_inc <- function(inc = 48, pop = 1e5, p_exp = 0.39, dog_inc = 0.01) {
+  1/(inc/pop/dog_inc/p_exp)
+}
+
+inc_from_hdr <- function(hdr = 5, pop = 1e5, p_exp = 0.39, dog_inc = 0.01) {
+  p_exp*dog_inc*pop/hdr
+}
+
+
+# Function for getting vials from input bites -------------------------------------------------
 get.vials <- function(x) {
   day0 <- round(runif(x, min = 1, max = 365))
   days <- data.table(days = c(day0, day0 + 3, day0 + 7))
@@ -133,9 +184,8 @@ get.vials <- function(x) {
               throughput = mean(days[, .(.N), by = days]$N, na.rm = TRUE)))
 }
 
-##' Helper Functions 
-##' ------------------------------------------------------------------------------------------------
-## Function for getting bootstrapped CIS
+# Helper Functions -----------------------------------------------------------------------------
+# Function for getting bootstrapped CIS
 boots_mean <- function(data, indices) {mean(data[indices], na.rm = TRUE)}
 boots_median <- function(data, indices) {median(data[indices], na.rm = TRUE)}
 
