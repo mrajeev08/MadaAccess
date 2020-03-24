c51f695feefeb2a74866e3611b5b24e2722f4c41
Malavika Rajeev <mrajeev08@gmail.com>
2020-03-24 10:31:02 -0400

    with bez option

--- R/figures/SM5_shifts.R
+++ R/figures/SM5_shifts.R
@@ -1,11 +1,9 @@
-####################################################################################################
-##' Plotting scenario shifts with ggridges
-##' Details: Pulling in district and commune estimates of travel times as clinics are added 
-##' Author: Malavika Rajeev 
-####################################################################################################
-rm(list=ls())
+# ------------------------------------------------------------------------------------------------ #
+#' Plotting scenario shifts        
+#' Pulling in district and commune estimates of travel times as clinics are added 
+# ------------------------------------------------------------------------------------------------ #
 
-##' Libraries and packages
+# Libraries and packages
 library(data.table)
 library(tidyverse)
 library(rgdal)
@@ -14,33 +12,41 @@ library(patchwork)
 library(raster)
 library(rasterVis)
 library(cowplot)
+library(ggforce)
 library(gdistance)
 library(foreach)
 select <- dplyr::select
 source("R/functions/ttime_functions.R")
+source("R/functions/out.session.R")
+source("R/functions/bezier.R")
 
-## District travel times
+# Pull in data
 ctar_metadata <- read.csv(file = "data/raw/ctar_metadata.csv")
-commune_master <- fread("output/ttimes/master_commune.csv")
-district_master <- fread("output/ttimes/master_district.csv")
-mada_districts <- readOGR("data/processed/shapefiles/mada_districts.shp")
-mada_communes <- readOGR("data/processed/shapefiles/mada_communes.shp")
+mada_districts <- readOGR("data/processed/shapefiles/mada_districts_simple.shp")
+mada_communes <- readOGR("data/processed/shapefiles/mada_communes_simple.shp")
 friction_masked <- raster("data/processed/rasters/friction_mada_masked.tif")
-base_times <- raster("output/ttimes/baseline_ttimes.tif")
+base_times <- raster("output/ttimes/base_ttimes.tif")
 pop_1x1 <- raster("data/processed/rasters/wp_2015_1x1.tif")
 
-## Single catchment
+# Commune/District preds
+commune_maxcatch <- fread("output/ttimes/commune_maxcatch.gz")
+district_maxcatch <- fread("output/ttimes/district_maxcatch.gz")
+commune_allcatch <- fread("output/ttimes/commune_allcatch.gz")
+district_allcatch <- fread("output/ttimes/district_allcatch.gz")
+
+# Single catchment
 district_master$scale <- "District"
 commune_master$scale <- "Commune"
-scenario_to_plot <- rbind(district_master, commune_master, fill = TRUE)
+scenario_to_plot <- rbind(district_maxcatch, commune_maxcatch, fill = TRUE)
 
-## colors 
+# colors 
 scale_levs <- c("Commune", "District")
 model_cols <- c("#0B775E", "#35274A")
 names(model_cols) <- scale_levs 
 
-# Catchmaps? ----------------------------------------------------------------------------------
-bites_by_catch <- fread("output/preds/catch_preds.gz")
+# Clinics added ------------------------------------------------------------------------------
+prop_df <- fread("output/ttimes/addclinics_prop_df.csv")
+prop_df$step_added <- 1:nrow(prop_df)
 
 # join w/ csb + ctar points
 ctar_metadata <- read.csv("data/raw/ctar_metadata.csv")
@@ -49,59 +55,83 @@ ctar_metadata <- read.csv("data/raw/ctar_metadata.csv")
 csbs <- read.csv("data/raw/csbs.csv", stringsAsFactors = FALSE)
 csbs %>% 
   filter(type == "CSB2", genre_fs != "Priv", type_fs != "Health Post") %>%
-  dplyr::select(CTAR = nom_fs, X_COORD = ycoor, Y_COORD = xcoor) -> csbs
+  dplyr::select(CTAR = nom_fs, lat = ycoor, long = xcoor) -> csbs
 
 point_mat_all <- rbind(dplyr::select(ctar_metadata, long = LONGITUDE, lat = LATITUDE),
-                       dplyr::select(csbs, long = Y_COORD, lat = X_COORD))
-point_mat_all$catchment <- 1:nrow(point_mat_all)
+                       dplyr::select(csbs, long, lat))
+point_mat_all$clinic_id <- 1:nrow(point_mat_all)
+point_mat_all %>%
+  left_join(prop_df) %>%
+  mutate(prop_pop = case_when(is.na(prop_pop) & clinic_id < 32 ~ 0,
+                              is.na(prop_pop) & clinic_id > 32 ~ 1e-12,
+                              !is.na(prop_pop) ~ prop_pop),
+         step_added = case_when(is.na(step_added) & clinic_id < 32 ~ 0,
+                                is.na(step_added) & clinic_id > 32 ~ 923,
+                                !is.na(step_added) ~ as.numeric(step_added))) -> point_mat_all
+
+gg_commune <- fortify(mada_communes, region = "commcode")
+
+S5.1_ARMCadded <- ggplot() +
+  geom_polygon(data = gg_commune, aes(x = long, y = lat, group = group), fill = "black") +
+  geom_point(data = filter(point_mat_all, step_added > 0),
+             aes(x = long, y = lat, size = prop_pop, fill = step_added), color = "grey50", 
+             alpha = 0.75, shape = 21) +
+  geom_point(data = filter(point_mat_all, step_added == 0),
+             aes(x = long, y = lat), shape = 3, size = 2, stroke = 1.2, color = "white") +
+  coord_quickmap() +
+  scale_fill_distiller(trans = "sqrt", direction = -1, palette = "PuRd",
+                        breaks = c(1, 100, 200, 400, 923), name = "# clinic added") +
+  scale_size_continuous(name = "Proportion of pop \n (travel times reduced < 3 hrs)") +
+  theme_map()
+
+ggsave("figs/supplementary/S5.1_ARMCadded.jpeg", S5.1_ARMCadded, width = 6, height = 8)
+
+# Clinics added ------------------------------------------------------------------------------
+bites_by_catch <- fread("output/preds/catch_preds.gz")
+props_by_catch <- fread("output/preds/catch_props.csv")
+scenarios <- unique(bites_by_catch$scenario)
+scenarios <- scenarios[scenarios %in% c(0, 100, 200, 400, sort(scenarios)[length(scenarios) - 1], 
+                                        max(scenarios))]
+bites_by_catch <- bites_by_catch[scenario %in% scenarios]
+props_by_catch <- props_by_catch[scenario %in% scenarios]
 bites_by_catch %>%
-  left_join(point_mat_all) -> bites_by_catch
+  left_join(point_mat_all, by = c("catchment" = "clinic_id")) -> bites_by_catch # clinic points
 
+# left join with props by catch
+clinic_bites <- left_join(bites_by_catch, props_by_catch) # need to make sure this is joined with clinic id as well!
+clinic_bites$line_size <- clinic_bites$prop_bites*clinic_bites$bites_mean
+clinic_bites <- left_join(clinic_bites, select(mada_communes@data, commcode, long_cent, lat_cent))
+bez_pts <- get.bezier.pts(from = data.frame(long = clinic_bites$long, 
+                                            lat = clinic_bites$lat),
+                          to = data.frame(long = clinic_bites$long_cent, 
+                                          lat = clinic_bites$lat_cent), 
+                          frac = 0.5, transform = function(x) sqrt(1/x)*0.1) 
+clinic_bites$group <- 1:nrow(clinic_bites)
+clinic_lines <- left_join(clinic_bites, bez_pts, by = c("group" = "group"))
+clinic_lines %>%
+  group_by(commcode, catchment) %>%
+  arrange(index) -> clinic_lines
 
-
-
-##' Plotting where clinics are added 
-##' ------------------------------------------------------------------------------------------------
-## candidate points
-csbs <- read.csv("data/raw/csbs.csv", stringsAsFactors = FALSE)
-csbs %>% 
-  filter(type == "CSB2", genre_fs != "Priv", type_fs != "Health Post") %>%
-  dplyr::select(CTAR = nom_fs, lat = ycoor, long = xcoor) %>%
-  mutate(candidate_id = 1:nrow(.) + 31) -> csbs
-
-pts <- SpatialPoints(cbind(csbs$long, csbs$lat), 
+# Filter out commune points
+pts <- SpatialPoints(cbind(bites_by_catch$long, bites_by_catch$lat), 
                      proj4string = 
                        CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-csbs$commcode <- over(pts, mada_communes)$commcode
-csbs$distcode <- over(pts, mada_districts)$distcode
+bites_by_catch$commcode_ctar <- over(pts, mada_communes)$commcode
 
-commune_master %>%
-  group_by(clinic_added) %>%
-  summarize(when_added = min(scenario)) %>%
-  right_join(csbs, by = c("clinic_added" = "candidate_id")) %>%
-  mutate(when_added = ifelse(is.na(when_added), 1648, when_added)) -> when_added
 
-pts <- SpatialPoints(cbind(ctar_metadata$LONGITUDE, ctar_metadata$LATITUDE), 
-                     proj4string = 
-                       CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-ctar_metadata$commcode <- over(pts, mada_communes)$commcode
-ctar_metadata$distcode <- over(pts, mada_districts)$distcode
-
-ctar_metadata %>%
-  select(CTAR, lat = LATITUDE, long = LONGITUDE, commcode, distcode) %>%
-  mutate(clinic_added = 1:31, when_added = 0) %>%
-  bind_rows(when_added) -> all_added
-  
-when_added %>%
-  group_by(commcode) %>%
-  summarize(when_added = min(when_added)) -> when_comm
-when_added %>%
-  group_by(distcode) %>%
-  summarize(when_added = min(when_added)) -> when_dist
-mada_communes$when_added <- when_comm$when_added[match(mada_communes$commcode, when_comm$commcode)]
-# mada_communes$when_added[is.na(mada_communes$when_added)] <- 473
-mada_districts$when_added <- when_dist$when_added[match(mada_districts$distcode, when_dist$distcode)]
-# mada_districts$when_added[is.na(mada_districts$when_added)] <- 473
+check <- ggplot() +
+  geom_polygon(data = gg_commune, aes(x = long, y = lat, group = group), fill = "grey") +
+  geom_segment(data = clinic_bites, 
+               aes(x = long, y = lat, xend = long_cent, yend = lat_cent, color = bites_mean,
+                   size = log(ceiling(prop_bites*bites_mean + 1.1))*0.15),
+               alpha = 0.5) +
+  geom_point(data = bites_by_catch,
+             aes(x = long, y = lat, size = log(bites_mean)*0.5, fill = bites_mean), 
+             color= "white", shape = 21) +
+  scale_size_identity() +
+  scale_color_distiller(aesthetics = c("color", "fill")) +
+  coord_quickmap() +
+  facet_wrap(~scenario)
 
 gg_commune <- fortify(mada_communes, region = "commcode")
 gg_commune %>% 
@@ -118,35 +148,8 @@ clinic_brks <- c(0, 10, 50, 100, 200, 400, 472, 1648, Inf)
 clinic_labs <- c("10", "50", "100", "200", "400", "472", "max", "None added")
 names(clinic_cols) <- clinic_labs
 
-S5.1A <- ggplot() +
-  geom_polygon(data = gg_district_plot,
-               aes(x = long, y = lat, group = group, 
-                   fill = cut(when_added, breaks = clinic_brks, labels = clinic_labs)), 
-               color = NA) +
-  geom_point(data = ctar_metadata, aes(x = LONGITUDE, y = LATITUDE), color = "grey50",
-             shape = 4, size = 2, stroke = 1.5) +
-  labs(tag = "A") +
-  scale_fill_manual(values = clinic_cols, name = "When clinic added") +
-  guides(fill = "none") +
-  theme_void()
 
-S5.1B <- ggplot() +
-  geom_polygon(data = gg_commune_plot,
-               aes(x = long, y = lat, group = group, 
-                   fill = cut(when_added, breaks = clinic_brks, labels = clinic_labs)), 
-               color = NA) +
-  geom_point(data = ctar_metadata, aes(x = LONGITUDE, y = LATITUDE, shape = 4), color = "grey50",
-             size = 2, stroke = 1.5) +
-  labs(tag = "B") +
-  scale_fill_manual(values = clinic_cols, name = "When clinic added") +
-  scale_shape_identity(guide = "legend", name = "", labels = "Existing ARMC") +
-  theme_void()
-
-S5.1 <- S5.1A | S5.1B
-ggsave("figs/supplementary/S5.1.jpeg", S5.1, height = 5, width = 7)
-
-##' Plots of how ttimes change 
-##' ------------------------------------------------------------------------------------------------
+# Plots of how ttimes change ----------------------------------------------------------------
 scenario_to_plot %>%
   filter(scenario %in% c(0, 100, 200, 300, 472, max(scenario_to_plot$scenario)), 
          scale == "District") %>%
@@ -174,7 +177,7 @@ gg_commune_plot$scenario <- factor(gg_commune_plot$name)
 levels(gg_commune_plot$scenario) <- list("baseline" = "0", "100" = "100", "200" = "200",
                                           "300" = "300", "472" = "472", "max (1648)" = "1648")
 
-## Getting ttime rasters
+# Getting ttime rasters
 steps <- c(0, 100, 200, 300, 472, max(scenario_to_plot$scenario))
 all_times <- base_times
 all_pts <- filter(all_added, when_added == 0)
@@ -242,14 +245,13 @@ S5.2C <- ggplot() +
 
 S5.2 <- S5.2A / S5.2B / S5.2C
 
-## Save as pdf otherwise too slow! 
+# Save as pdf otherwise too slow! 
 # ggsave("figs/supplementary/S5.2.jpeg", dpi = 300, S5.2, height = 7, width = 7)
 ggsave("figs/supplementary/S5.2.tiff", S5.2, height = 7, width = 7, compression = "lzw",
        device = "tiff", dpi = 300, type = "cairo")
 # ggsave("figs/supplementary/S5.2.pdf", S5.2, height = 7, width = 7)
 
-##' Histogram of proportion of people living within x hours
-##' ------------------------------------------------------------------------------------------------
+# Histogram of proportion of people living within x hours ---------------------------------------
 pop_total <- sum(getValues(pop_1x1), na.rm = TRUE)
 all_times_df %>%
   filter(!is.na(pop), !is.infinite(ttimes), !is.na(ttimes)) %>%
@@ -274,9 +276,8 @@ S5.3 <- ggplot(data = prop_pop_ttimes, aes(x = cut_times, y = prop_pop)) +
 
 ggsave("figs/supplementary/S5.3.jpeg", S5.3, height = 5, width = 7)
 
-##' Shifts in proportion of people in single catchment
-##' ------------------------------------------------------------------------------------------------
-##' Single catchment
+# Shifts in proportion of people in single catchment -----------------------------------------
+# Single catchment
 scenario_filtered <- filter(scenario_to_plot, scenario %in% c(0, 100, 200, 300, 472,
                                                               max(scenario_to_plot$scenario)))
 
@@ -291,8 +292,7 @@ S5.4 <- ggplot(data = scenario_filtered, aes(x = prop_pop_catch, y = as.factor(s
 
 ggsave("figs/supplementary/S5.4.jpeg", S5.4, height = 5, width = 7)
 
-##' Decreases in ttimes, bite incidence, and reporting (Fig S5.4)
-##' ------------------------------------------------------------------------------------------------
+# Decreases in ttimes, bite incidence, and reporting (Fig S5.4) -----------------------------
 scenario_to_plot$scenario[scenario_to_plot$scenario == max(scenario_to_plot$scenario)] <- 600
 scenario_to_plot %>%
   group_by(scenario, scale) %>%
@@ -300,7 +300,7 @@ scenario_to_plot %>%
             ttimes_lower25 = quantile(weighted_times/60, probs = 0.25),
             ttimes_upper75 = quantile(weighted_times/60, probs = 0.75)) -> scenario_ttimes
 
-## Shift in travel times
+# Shift in travel times
 S5.5A <- ggplot(data = filter(scenario_ttimes, scenario != 600), 
             aes(x = scenario, y = ttimes_mean, color = scale, fill = scale)) +
   geom_line(size = 1) +
@@ -322,14 +322,14 @@ admin_preds <- fread("output/preds/complete/burden_filled.csv")
 admin_preds$scenario[admin_preds$scenario == max(admin_preds$scenario)] <- 600
 admin_preds  %>%
   group_by(scenario, scale) %>%
-  summarize(reporting_upper75 = quantile(reporting_mean, probs = 0.25),
-            reporting_lower25 = quantile(reporting_mean, probs = 0.75),
+  summarize(reporting_upper75 = quantile(reporting_mean, probs = 0.025),
+            reporting_lower25 = quantile(reporting_mean, probs = 0.975),
             reporting_mean = mean(reporting_mean), 
-            bites_upper75 = quantile(bites_mean/pop*1e5, probs = 0.25),
-            bites_lower25 = quantile(bites_mean/pop*1e5, probs = 0.75), 
+            bites_upper75 = quantile(bites_mean/pop*1e5, probs = 0.025),
+            bites_lower25 = quantile(bites_mean/pop*1e5, probs = 0.975), 
             bites_mean = mean(bites_mean/pop*1e5), 
-            deaths_upper75 = quantile(deaths_mean/pop*1e5, probs = 0.25),
-            deaths_lower25 = quantile(deaths_mean/pop*1e5, probs = 0.75),
+            deaths_upper75 = quantile(deaths_mean/pop*1e5, probs = 0.025),
+            deaths_lower25 = quantile(deaths_mean/pop*1e5, probs = 0.975),
             deaths_mean = mean(deaths_mean/pop*1e5)) -> trend_preds
 
 S5.5B <- ggplot(data = filter(trend_preds, scenario != 600), 
@@ -391,8 +391,7 @@ trend_preds %>%
 write.csv(trend_preds, "output/stats/trend_preds.csv", row.names = FALSE)
 
 
-##' Shifts in predicted bites, reporting, deaths at specific steps
-##' ------------------------------------------------------------------------------------------------
+# Shifts in predicted bites, reporting, deaths at specific steps -------------------------------
 preds_filtered <- filter(admin_preds, scenario %in% c(0, 100, 200, 300, 472,
                                                               max(admin_preds$scenario)))
 
@@ -440,10 +439,9 @@ S5.6D <- ggplot() +
 S5.6 <- S5.6A + S5.6B + S5.6C + S5.6D + plot_layout(nrow = 2, guides = "collect")
 ggsave("figs/supplementary/S5.6.jpeg", S5.6, height = 7, width = 7)
 
-##' Shifts @ clinic level average daily throughput + vials needed
-##' ------------------------------------------------------------------------------------------------
-##' Shifts in catchment populations 
-## Commune only because district maxes at 1 clinic per district
+# Shifts @ clinic level average daily throughput + vials needed -------------------------------
+# Shifts in catchment populations 
+# Commune only because district maxes at 1 clinic per district
 master_commune <- fread("output/ttimes/master_commune.csv")
 master_commune %>%
   group_by(base_catches, scenario) %>%
@@ -455,7 +453,7 @@ master_commune %>%
             catch_pop_mean = mean(catch_pop)) -> pop_by_catch_comm
 pop_by_catch_comm$scenario[pop_by_catch_comm$scenario == max(pop_by_catch_comm$scenario)] <- 600
 
-## Plot means
+# Plot means
 S5.7A <- ggplot(data = filter(pop_by_catch_comm, scenario != 600), 
                 aes(x = scenario, y = catch_pop_mean), color = model_cols["Commune"]) +
   geom_line(size = 1) +
@@ -473,10 +471,10 @@ S5.7A <- ggplot(data = filter(pop_by_catch_comm, scenario != 600),
   theme_minimal_hgrid() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-## Output for pulling in stats
+# Output for pulling in stats
 write.csv(pop_by_catch_comm, "output/stats/pop_by_catch.csv", row.names = FALSE)
 
-## Throughput + vials
+# Throughput + vials
 vial_preds <- fread("output/preds/complete/vials_filled.csv")
 vial_preds %>%
   group_by(scenario, scale) %>%
@@ -527,3 +525,7 @@ S5.7C <- ggplot(data = filter(vials_summed, scenario != 600),
 
 S5.7 <- S5.7A / S5.7B / S5.7C + plot_layout(guides = "collect")
 ggsave("figs/supplementary/S5.7.jpeg", S5.7, height = 12, width = 6)
+
+# Session Info
+out.session(path = "R/SM5?.R", filename = "output/log_local.csv")
+
