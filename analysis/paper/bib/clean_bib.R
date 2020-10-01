14041a512fb7641bd488c8bd37f2b6d82a1d77b1
Malavika Rajeev <mrajeev08@gmail.com>
2020-10-01 10:37:15 -0700

    cleaning up bib manually

--- analysis/paper/bib/clean_bib.R
+++ analysis/paper/bib/clean_bib.R
@@ -1,4 +1,4 @@
-## Clean up bib files
+# Clean up bib files
 library(citr)
 tidy_bib_file("docs/manuscript.rmd", "docs/bib/refs_all.bib", file = "docs/bib/refs_all.bib",
               encoding = getOption("citr.encoding"))
